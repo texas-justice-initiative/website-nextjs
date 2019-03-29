aa96bbd9fc5ddfd043ba793d2e52e50559235d28
sheadscott <sheadscott@gmail.com>
2019-03-29 15:51:00 -0500

    Updated README

--- pages/chart-test.js
+++ pages/chart-test.js
@@ -1,18 +1,16 @@
-import React from "react";
-import VictoryDeathsByYear from "../components/charts/victory/DeathsByYear";
-import ChartJSDeathsByYear from "../components/charts/chartsjs/DeathsByYear";
+import React from 'react';
+import VictoryDeathsByYear from '../components/charts/victory/DeathsByYear';
+import ChartJSDeathsByYear from '../components/charts/chartsjs/DeathsByYear';
 
-const Index = () => {
-  return (
+const Index = () => (
+  <div>
     <div>
-      <div>
-        <VictoryDeathsByYear title="Victory - Deaths By Year" />
-      </div>
-      <div>
-        <ChartJSDeathsByYear title="ChartsJS - Deaths By Year" />
-      </div>
+      <VictoryDeathsByYear title="Victory - Deaths By Year" />
     </div>
-  );
-};
+    <div>
+      <ChartJSDeathsByYear title="ChartsJS - Deaths By Year" />
+    </div>
+  </div>
+);
 
 export default Index;
