edabaf94c9a88430ab6d0eb83f22172c7f7685f6
sheadscott <sheadscott@gmail.com>
2019-04-18 16:46:04 -0500

    Added contact form page

--- pages/chart-test.js
+++ pages/chart-test.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Head from 'next/head';
 import VictoryDeathsByYear from '../components/charts/victory/DeathsByYear';
 import ChartJSDeathsByYear from '../components/charts/chartsjs/DeathsByYear';
 
+const pageTitle = 'Chart Test';
+
 const Index = () => (
   <div>
+    <Head>
+      <title>Texas Justice Initiative | {pageTitle}</title>
+    </Head>
     {/* <div>
       <VictoryDeathsByYear title="Victory - Deaths By Year" />
     </div> */}
