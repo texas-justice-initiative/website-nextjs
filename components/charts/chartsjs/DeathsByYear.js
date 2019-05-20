28948ceac6a840d5523d42fcfff76613f779acdc
sheadscott <sheadscott@gmail.com>
2019-05-19 21:54:55 -0500

    Finished data processing based on user selected filter

--- components/charts/chartsjs/DeathsByYear.js
+++ components/charts/chartsjs/DeathsByYear.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 
+const calculateYearData = (year, yearData) => {
+  const filterItems = (arr, query) => arr.filter(year => year === query);
+  // Calculate the total # of deaths per year
+  // if value is null return 0 otherwise return total # of deaths for that year
+  const deathsByYearData = year.map((yearValue, index) => (!yearValue ? 0 : filterItems(yearData, index).length));
+
+  return {
+    // Count from 2005 to get the label values
+    labels: year.map((year, index) => index + 2005),
+    datasets: [
+      {
+        label: 'Deaths By Year',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: deathsByYearData,
+      },
+    ],
+  };
+};
+
 const DeathsByYear = props => {
-  const { title, yearData } = props;
+  const { title, meta, yearData } = props;
+  const data = calculateYearData(meta, yearData);
   return (
     <React.Fragment>
       <h2>{title}</h2>
-      <Bar data={yearData} />
+      <Bar data={data} />
     </React.Fragment>
   );
 };
--- pages/data.js
+++ pages/data.js
@@ -5,97 +5,115 @@ import data from '../data/cdr_compressed';
 import CheckboxGroup from '../components/CheckboxGroup';
 import ChartJSDeathsByYear from '../components/charts/chartsjs/DeathsByYear';
 
-console.log('DATA: ', data);
 // Initialize data values
-const yearsInit = data.meta.lookups.year;
-const racesInit = data.meta.lookups.race;
-const sexesInit = data.meta.lookups.sex;
-const mannerOfDeathsInit = data.meta.lookups.manner_of_death;
-const typesOfCustodyInit = data.meta.lookups.type_of_custody;
-const deathLocationTypesInit = data.meta.lookups.death_location_type;
-const meansOfDeathsInit = data.meta.lookups.means_of_death;
+const {
+  age_at_time_of_death,
+  agency_name,
+  death_location_county,
+  death_location_type,
+  manner_of_death,
+  means_of_death,
+  race,
+  sex,
+  type_of_custody,
+  year,
+} = data.meta.lookups;
+
+// get values to generate option list
+const {
+  age_at_time_of_death: ageInit,
+  agency_name: agencyInit,
+  death_location_county: countyInit,
+  death_location_type: locTypeInit,
+  manner_of_death: mannerInit,
+  means_of_death: meansInit,
+  race: raceInit,
+  sex: sexInit,
+  type_of_custody: typeOfCustodyInit,
+  year: yearInit,
+} = data.meta.lookups;
 
 class Explore extends Component {
   state = {
-    years: yearsInit,
-    yearData: {},
-    races: racesInit,
-    sexes: sexesInit,
-    mannerOfDeaths: mannerOfDeathsInit,
-    typesOfCustody: typesOfCustodyInit,
-    deathLocationTypes: deathLocationTypesInit,
-    meansOfDeaths: meansOfDeathsInit,
+    age_at_time_of_death,
+    agency_name,
+    death_location_county,
+    death_location_type,
+    manner_of_death,
+    means_of_death,
+    race,
+    sex,
+    type_of_custody,
+    year,
+    currentData: data.records,
+    recordCount: data.meta.num_records,
   };
 
-  componentDidMount() {
-    this.calculateYearData();
-  }
+  calculateData = type => {
+    // Not sure why type is undefined, or why this function is called twice ???
 
-  calculateYearData = () => {
-    const { years } = this.state;
-    const deaths = data.records.year;
-    const filterItems = (arr, query) => arr.filter(year => year === query);
-    const deathsByYearData = years.sort().map((year, index) => filterItems(deaths, index).length);
-    const yearData = {
-      labels: years,
-      datasets: [
-        {
-          label: 'Deaths By Year',
-          backgroundColor: 'rgba(255,99,132,0.2)',
-          borderColor: 'rgba(255,99,132,1)',
-          borderWidth: 1,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
-          data: deathsByYearData,
-        },
-      ],
-    };
-    this.setState({ yearData });
+    if (type) {
+      // return only records when the filter value isn't null
+      // ##### Filter all data based on user selections
+
+      let removalList = [];
+
+      Object.keys(data.records).forEach(key => {
+        // Iterate through each of the items in data.records
+        data.records[key].filter((value, index) => {
+          // Filter based on the values in the user selected options
+          if (this.state[key][value] !== null) return true;
+          // Add to removalList if
+          removalList.push(index);
+          return false;
+        });
+      });
+
+      // Get unique array to remove ie. get rid of duplicates
+      removalList = [...new Set(removalList)];
+      // console.log(removalList);
+
+      const currentData = {};
+
+      // Remove filtered values from all data
+      Object.keys(data.records).forEach(key => {
+        // console.log(key);
+        const category = data.records[key];
+        const filteredData = category.filter((val, index) => removalList.indexOf(index) < 0);
+        currentData[key] = filteredData;
+      });
+
+      // set state to filtered data
+      this.setState({ currentData });
+      // console.log(currentData);
+      // console.log('DATA: ', data.records);
+    }
   };
 
   handleCheckboxChange = e => {
+    this.calculateData();
     // Get type of checkbox
     const type = e.target.name;
-
-    // Update state based on which category has changed
-    const updateChart = chartType => {
-      switch (chartType) {
-        case 'year':
-          this.calculateYearData();
-          break;
-        default:
-          break;
-      }
-    };
-    const pluralObject = {
-      year: 'years',
-      sex: 'sexes',
-      race: 'races',
-      mannerOfDeath: 'mannerOfDeaths',
-      typeOfCustody: 'typesOfCustody',
-      deathLocationType: 'deathLocationTypes',
-      meansOfDeath: 'meansOfDeaths',
-    };
-    const category = pluralObject[type];
-    const value = category === 'years' ? parseInt(e.target.value) : e.target.value;
-    const values = [...this.state[category]];
+    const value = type === 'year' ? parseInt(e.target.value) : e.target.value;
+    const values = [...this.state[type]];
 
     if (!e.target.checked) {
       if (values.includes(value)) {
-        values.splice(values.indexOf(value), 1);
+        values[values.indexOf(value)] = null;
       }
     } else if (!values.includes(value)) {
-      values.push(value);
+      values[data.meta.lookups[type].indexOf(value)] = value;
     }
-    this.setState({ [category]: values }, () => {
-      // Update chart after state changes
-      updateChart(type);
+    // console.log('VALUES: ', values);
+    this.setState({ [type]: values }, () => {
+      // calculate dataset after change
+      this.calculateData(type);
     });
   };
 
   render() {
     const pageTitle = 'Explore the Data';
-    const { years, races, sexes, mannerOfDeaths, yearData } = this.state;
+    const { year, race, sex, manner_of_death, currentData, recordCount } = this.state;
     return (
       <Wrapper>
         <Head>
@@ -103,28 +121,25 @@ class Explore extends Component {
         </Head>
         <Aside>
           <form action="">
-            <CheckboxGroup name="year" values={yearsInit} handler={this.handleCheckboxChange} />
-            <CheckboxGroup name="race" values={racesInit} handler={this.handleCheckboxChange} />
-            <CheckboxGroup name="sex" values={sexesInit} handler={this.handleCheckboxChange} />
-            <CheckboxGroup name="mannerOfDeath" values={mannerOfDeathsInit} handler={this.handleCheckboxChange} />
-            <CheckboxGroup name="typeOfCustody" values={typesOfCustodyInit} handler={this.handleCheckboxChange} />
-            <CheckboxGroup
-              name="deathLocationType"
-              values={deathLocationTypesInit}
-              handler={this.handleCheckboxChange}
-            />
-            <CheckboxGroup name="meansOfDeath" values={meansOfDeathsInit} handler={this.handleCheckboxChange} />
+            <CheckboxGroup name="year" values={yearInit} handler={this.handleCheckboxChange} />
+            <CheckboxGroup name="race" values={raceInit} handler={this.handleCheckboxChange} />
+            <CheckboxGroup name="sex" values={sexInit} handler={this.handleCheckboxChange} />
+            <CheckboxGroup name="manner_of_death" values={mannerInit} handler={this.handleCheckboxChange} />
+            <CheckboxGroup name="type_of_custody" values={typeOfCustodyInit} handler={this.handleCheckboxChange} />
+            <CheckboxGroup name="death_location_type" values={locTypeInit} handler={this.handleCheckboxChange} />
+            <CheckboxGroup name="means_of_death" values={meansInit} handler={this.handleCheckboxChange} />
           </form>
         </Aside>
         <Main>
           <h1>{pageTitle}</h1>
+          <h2>Total number of filtered incidents: {recordCount}</h2>
           <div>
-            <ChartJSDeathsByYear title="ChartsJS - Deaths By Year" yearData={yearData} />
+            <ChartJSDeathsByYear title="ChartsJS - Deaths By Year" meta={year} yearData={currentData.year} />
           </div>
-          <p>Years: {years.map(year => `${year}, `)}</p>
-          <p>Race: {races.map(race => `${race}, `)}</p>
-          <p>Sex: {sexes.map(sex => `${sex}, `)}</p>
-          <p>Manner of Death: {mannerOfDeaths.map(death => `${death}, `)}</p>
+          <p>Years: {year.map(year => `${year}, `)}</p>
+          <p>Race: {race.map(race => `${race}, `)}</p>
+          <p>Sex: {sex.map(sex => `${sex}, `)}</p>
+          <p>Manner of Death: {manner_of_death.map(death => `${death}, `)}</p>
         </Main>
       </Wrapper>
     );
@@ -135,6 +150,10 @@ export default Explore;
 
 const Wrapper = styled.div`
   display: flex;
+
+  h2 {
+    margin: 2rem 0;
+  }
 `;
 
 const Aside = styled.aside`
