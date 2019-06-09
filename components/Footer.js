cff9da1392e77cac9fc58c054f3c0f7015d07bd1
Jen Udan <jen.udan@gmail.com>
2019-06-09 00:35:43 -0500

    Added social links to footer

--- components/Footer.js
+++ components/Footer.js
@@ -10,6 +10,19 @@ class Footer extends Component {
         <p className="newsletter-cta">Subscribe to Our Newsletter</p>
         <MailChimpForm />
 
+        <div className="social-links">
+          <a
+            href="https://www.facebook.com/TXJusticeInitiative"
+            target="_blank"
+            title="Visit Texas Justice Initiative on Facebook"
+          >
+            <img src={require('../images/tji-fb-icon.svg')} alt="TJI Facebook" />
+          </a>
+          <a href="https://twitter.com/JusticeTexas" target="_blank" title="Visit Texas Justice Initiative on Twitter">
+            <img src={require('../images/tji-twitter-icon.svg')} alt="TJI Facebook" />
+          </a>
+        </div>
+
         <div id="logo">
           <Link href="/">
             <img src={require('../images/tji-logo.svg')} alt="TJI Logo" />
@@ -50,6 +63,14 @@ const StyledFooter = styled.footer`
     p {
       margin: 0;
     }
+    a {
+      text-decoration: underline;
+      font-weight: 700;
+    }
+  }
+  .social-links img {
+    height: 40px;
+    margin-right: 10px;
   }
 `;
 
