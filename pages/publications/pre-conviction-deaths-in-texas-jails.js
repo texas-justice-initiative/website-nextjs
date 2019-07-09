01f7cebcbcf8658854cdf90e50f378ef179e9b8e
gendirection-jzinn <jason@zinnfinity.com>
2019-07-09 11:36:05 -0500

    Added unique Tableau component

---
+++ pages/publications/pre-conviction-deaths-in-texas-jails.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+import Primary from '../../components/Primary';
+import TableauReport from '../../components/TableauReport';
+
+const pageTitle = 'Pre-Conviction Deaths in Texas Jails Since 2005';
+
+const Page = () => (
+  <React.Fragment>
+    <Head>
+      <title>Texas Justice Initiative | {pageTitle}</title>
+    </Head>
+    <Primary>
+      <h1>{pageTitle}</h1>
+
+      <TableauReport url='https://public.tableau.com/views/TJI-BailReform/Deaths_Bail' />
+    </Primary>
+  </React.Fragment>
+);
+export default Page;
