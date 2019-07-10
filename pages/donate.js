8696abfe6a7504daf9526bc519c9573937f7cff1
Jason Zinn <jason@zinnfinity.com>
2019-07-10 11:34:49 -0500

    Added sidebar to page

--- pages/donate.js
+++ pages/donate.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
 import Primary from '../components/Primary';
+import Sidebar from '../components/Sidebar';
 import PaypalExpressBtn from 'react-paypal-express-checkout';
 import AmountButton from '../components/forms/AmountButton';
 import AmountInput from '../components/forms/AmountInput';
@@ -57,6 +58,32 @@ class Page extends React.Component {
           </form>
           <PaypalExpressBtn client={client} currency={'USD'} total={1.0} />
         </Primary>
+        <Sidebar>
+          <h3>Our Mission</h3>
+          <p>
+            Collect, vet and publicly release information on criminal justice and policing in Texas while pushing for
+            improved transparency.
+          </p>
+
+          <h3>Our Vision</h3>
+          <p>
+            To give Texans the most dependable data and most complete picture of law enforcement in the state, enabling
+            better understanding.
+          </p>
+
+          <h3>Our Values</h3>
+
+          <p>We provide oversight of the data released by state and local governmental entities.</p>
+
+          <p>
+            We seek to improve understanding through presenting information in a rich context and combining a variety of
+            data.
+          </p>
+
+          <p>We hope to encourage the continuation of Texas’ leadership in transparency in policing and accountability.</p>
+
+          <p>We wish to give Texans of all creed more information on how law enforcement agencies and officers operate.</p>
+        </Sidebar>
       </React.Fragment>
     );
   }
