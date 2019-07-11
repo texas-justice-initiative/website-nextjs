2712dd8dca8d62a623dad611ead2b4d2cea80fcb
Jason Zinn <jason@zinnfinity.com>
2019-07-11 13:25:59 -0500

    Simplified form change handling

--- components/forms/DonationForm.js
+++ components/forms/DonationForm.js
@@ -6,22 +6,20 @@ class DonationForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: '',
+      email: '',
       donation: 0,
+      addTax: false,
+      nameValid: false,
+      emailValid: false,
+      amountValid: false,
+      formValid: false,
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleAmountChange = this.handleAmountChange.bind(this);
   }
 
   handleInputChange(event) {
-    const target = event.target;
-    const amount = target.value;
-    this.setState({
-      donation: amount,
-    });
-  }
-
-  handleAmountChange(event) {
     const target = event.target;
     const amount = target.value;
     this.setState({
@@ -48,19 +46,19 @@ class DonationForm extends React.Component {
           Email Address
           <input name="emailAddress" type="email" onChange={this.handleInputChange} />
         </label>
-        <button type="button" value="500" onClick={this.handleAmountChange}>
+        <button type="button" name="amountButton" value="500" onClick={this.handleInputChange}>
           $500
         </button>
-        <button type="button" value="250" onClick={this.handleAmountChange}>
+        <button type="button" name="amountButton" value="250" onClick={this.handleInputChange}>
           $250
         </button>
-        <button type="button" value="100" onClick={this.handleAmountChange}>
+        <button type="button" name="amountButton" value="100" onClick={this.handleInputChange}>
           $100
         </button>
-        <button type="button" value="50" onClick={this.handleAmountChange}>
+        <button type="button" name="amountButton" value="50" onClick={this.handleInputChange}>
           $50
         </button>
-        <button type="button" value="25" onClick={this.handleAmountChange}>
+        <button type="button" name="amountButton" value="25" onClick={this.handleInputChange}>
           %25
         </button>
         <div>
@@ -68,8 +66,8 @@ class DonationForm extends React.Component {
           <input name="otherAmount" type="text" pattern="\d+(\.\d{2})?" onChange={this.handleInputChange} />
         </div>
         <label htmlFor="includeTax">
-          <input name="includeTax" type="checkbox" />I would like to add 2.2% plus $0.30 to my donation to cover PayPal
-          processing costs.
+          <input name="includeTax" type="checkbox" onClick={this.handleInputChange} />I would like to add 2.2% plus
+          $0.30 to my donation to cover PayPal processing costs.
         </label>
         <PaypalExpressBtn client={client} currency="USD" total={this.state.donation} />
       </form>
