0c4bcb0b2e3c74e48d6183c724253906dd842767
Jen Udan <jen.udan@gmail.com>
2019-07-13 21:39:28 -0500

    Added fetch for CDRs on AWS

--- pages/data.js
+++ pages/data.js
@@ -1,54 +1,61 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
-import data from '../data/cdr_compressed';
+import fetch from 'isomorphic-unfetch';
 import CheckboxGroup from '../components/CheckboxGroup';
 import BarChart from '../components/charts/chartsjs/BarChart';
 import PieChart from '../components/charts/chartsjs/PieChart';
+import DeathsByDataType from '../components/charts/chartsjs/PieChart';
 
-// Initialize data values
-const {
-  age_at_time_of_death,
-  agency_name,
-  death_location_county,
-  death_location_type,
-  manner_of_death,
-  means_of_death,
-  race,
-  sex,
-  type_of_custody,
-  year,
-} = data.meta.lookups;
+// // Initialize data values
+// const {
+//   age_at_time_of_death,
+//   agency_name,
+//   death_location_county,
+//   death_location_type,
+//   manner_of_death,
+//   means_of_death,
+//   race,
+//   sex,
+//   type_of_custody,
+//   year,
+// } = data.meta.lookups;
 
-// get values to generate option list
-const {
-  age_at_time_of_death: ageInit,
-  agency_name: agencyInit,
-  death_location_county: countyInit,
-  death_location_type: locTypeInit,
-  manner_of_death: mannerInit,
-  means_of_death: meansInit,
-  race: raceInit,
-  sex: sexInit,
-  type_of_custody: typeOfCustodyInit,
-  year: yearInit,
-} = data.meta.lookups;
+// // get values to generate option list
+// const {
+//   age_at_time_of_death: ageInit,
+//   agency_name: agencyInit,
+//   death_location_county: countyInit,
+//   death_location_type: locTypeInit,
+//   manner_of_death: mannerInit,
+//   means_of_death: meansInit,
+//   race: raceInit,
+//   sex: sexInit,
+//   type_of_custody: typeOfCustodyInit,
+//   year: yearInit,
+// } = data.meta.lookups;
 
 class Explore extends Component {
-  state = {
-    age_at_time_of_death,
-    agency_name,
-    death_location_county,
-    death_location_type,
-    manner_of_death,
-    means_of_death,
-    race,
-    sex,
-    type_of_custody,
-    year,
-    currentData: data.records,
-    recordCount: data.meta.num_records.toLocaleString(),
-  };
+  static async getInitialProps() {
+    const res = await fetch('https://s3.amazonaws.com/tji-compressed-data/cdr_compressed.json');
+    const data = await res.json();
+    return { data };
+  }
+
+  // state = {
+  //   age_at_time_of_death,
+  //   agency_name,
+  //   death_location_county,
+  //   death_location_type,
+  //   manner_of_death,
+  //   means_of_death,
+  //   race,
+  //   sex,
+  //   type_of_custody,
+  //   year,
+  //   currentData: data.records,
+  //   recordCount: data.meta.num_records,
+  // };
 
   calculateData = type => {
     // Not sure why type is undefined, or why this function is called twice ???
@@ -59,9 +66,9 @@ class Explore extends Component {
 
       let removalList = [];
 
-      Object.keys(data.records).forEach(key => {
+      Object.keys(this.props.data.records).forEach(key => {
         // Iterate through each of the items in data.records
-        data.records[key].filter((value, index) => {
+        this.props.data.records[key].filter((value, index) => {
           // Filter based on the values in the user selected options
           if (this.state[key][value] !== null) return true;
           // Add to removalList if
@@ -77,9 +84,9 @@ class Explore extends Component {
       const currentData = {};
 
       // Remove filtered values from all data
-      Object.keys(data.records).forEach(key => {
+      Object.keys(this.props.data.records).forEach(key => {
         // console.log(key);
-        const category = data.records[key];
+        const category = this.props.data.records[key];
         const filteredData = category.filter((val, index) => removalList.indexOf(index) < 0);
         currentData[key] = filteredData;
       });
@@ -114,7 +121,20 @@ class Explore extends Component {
 
   render() {
     const pageTitle = 'Explore the Data';
-    const { year, race, sex, manner_of_death, currentData, recordCount } = this.state;
+    // const { year, race, sex, manner_of_death, currentData, recordCount } = this.state;
+    const {
+      age_at_time_of_death,
+      agency_name,
+      death_location_county,
+      death_location_type,
+      manner_of_death,
+      means_of_death,
+      race,
+      sex,
+      type_of_custody,
+      year,
+    } = this.props.data.meta.lookups;
+
     return (
       <Wrapper>
         <Head>
@@ -122,31 +142,31 @@ class Explore extends Component {
         </Head>
         <Aside>
           <form action="">
-            <CheckboxGroup name="year" values={yearInit} handler={this.handleCheckboxChange} />
-            <CheckboxGroup name="race" values={raceInit} handler={this.handleCheckboxChange} />
-            <CheckboxGroup name="sex" values={sexInit} handler={this.handleCheckboxChange} />
-            <CheckboxGroup name="manner_of_death" values={mannerInit} handler={this.handleCheckboxChange} />
-            <CheckboxGroup name="type_of_custody" values={typeOfCustodyInit} handler={this.handleCheckboxChange} />
-            <CheckboxGroup name="death_location_type" values={locTypeInit} handler={this.handleCheckboxChange} />
-            <CheckboxGroup name="means_of_death" values={meansInit} handler={this.handleCheckboxChange} />
+            <CheckboxGroup name="year" values={year} handler={this.handleCheckboxChange} />
+            <CheckboxGroup name="race" values={race} handler={this.handleCheckboxChange} />
+            <CheckboxGroup name="sex" values={sex} handler={this.handleCheckboxChange} />
+            <CheckboxGroup name="manner_of_death" values={manner_of_death} handler={this.handleCheckboxChange} />
+            <CheckboxGroup name="type_of_custody" values={type_of_custody} handler={this.handleCheckboxChange} />
+            <CheckboxGroup
+              name="death_location_type"
+              values={death_location_type}
+              handler={this.handleCheckboxChange}
+            />
+            <CheckboxGroup name="means_of_death" values={means_of_death} handler={this.handleCheckboxChange} />
           </form>
         </Aside>
         <Main>
           <h1>{pageTitle}</h1>
-          <h2>Total number of filtered incidents: {recordCount}</h2>
+          <h2>Total number of filtered incidents: {this.props.data}</h2>
           <div>
-            <BarChart title="Year" meta={year} metaData={currentData.year} />
-            <PieChart title="Race" meta={race} metaData={currentData.race} />
-            <PieChart title="Sex" meta={sex} metaData={currentData.sex} />
-            <PieChart title="Manner of Death" meta={manner_of_death} metaData={currentData.manner_of_death} />
-            <PieChart title="Age Group" meta={age_at_time_of_death} metaData={currentData.age_at_time_of_death} />
-            <PieChart title="Type of Custody" meta={type_of_custody} metaData={currentData.type_of_custody} />
-            <PieChart
-              title="Death Location Type"
-              meta={death_location_type}
-              metaData={currentData.death_location_type}
-            />
-            <PieChart title="Means of Death" meta={means_of_death} metaData={currentData.means_of_death} />
+            <BarChart title="Year" meta={year} metaData={year} />
+            <PieChart title="Race" meta={race} metaData={race} />
+            <PieChart title="Sex" meta={sex} metaData={sex} />
+            <PieChart title="Manner of Death" meta={manner_of_death} metaData={manner_of_death} />
+            <PieChart title="Age Group" meta={age_at_time_of_death} metaData={age_at_time_of_death} />
+            <PieChart title="Type of Custody" meta={type_of_custody} metaData={type_of_custody} />
+            <PieChart title="Death Location Type" meta={death_location_type} metaData={death_location_type} />
+            <PieChart title="Means of Death" meta={means_of_death} metaData={means_of_death} />
           </div>
         </Main>
       </Wrapper>
@@ -158,7 +178,6 @@ export default Explore;
 
 const Wrapper = styled.div`
   display: flex;
-
   h2 {
     margin: 2rem 0;
   }
