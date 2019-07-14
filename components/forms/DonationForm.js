294f8c105c943bd636ea6d2778982419686edc27
Jason Zinn <jason@zinnfinity.com>
2019-07-14 06:25:29 -0500

    Added basic callback functions for PayPal

--- components/forms/DonationForm.js
+++ components/forms/DonationForm.js
@@ -109,6 +109,24 @@ class DonationForm extends React.Component {
       production: 'YOUR-PRODUCTION-APP-ID',
     };
 
+    const onSuccess = (payment) => {
+      // Congratulation, it came here means everything's fine!
+      console.log('The payment was succeeded!', payment);
+      // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
+    }
+
+    const onCancel = (data) => {
+      // User pressed "cancel" or close Paypal's popup!
+      console.log('The payment was cancelled!', data);
+      // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
+    }
+
+    const onError = (err) => {
+      // The main Paypal's script cannot be loaded or somethings block the loading of that script!
+      console.log("Error!", err);
+      // Because the Paypal's main script is loaded asynchronously from "https://www.paypalobjects.com/api/checkout.js"
+      // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
+    }
     return (
       <Form className="donation-form">
         <div className="donation-form__row">
@@ -135,7 +153,6 @@ class DonationForm extends React.Component {
           <div>
             {donationAmounts.map(amount => {
               const selected = amount === parseInt(selectedAmount);
-              console.log('SELECTED: ', selected, amount);
               return (
                 <button
                   key={amount}
@@ -171,9 +188,9 @@ class DonationForm extends React.Component {
             commit={true}
             currency="USD"
             total={this.state.total}
-            onSuccess={console.log('success')}
-            onError={console.log('error')}
-            onCancel={console.log('cancel')}
+            onSuccess={onSuccess}
+            onError={onError}
+            onCancel={onCancel}
           />
         </div>
       </Form>
