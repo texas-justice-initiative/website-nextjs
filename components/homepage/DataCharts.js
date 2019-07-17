2bf3f3f2bdda9aa925a9752541e71c216f10740d
Jason Zinn <jason@zinnfinity.com>
2019-07-17 14:10:52 -0500

    Created basic components for homepage

---
+++ components/homepage/DataCharts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+import styled from 'styled-components';
+
+class DataCharts extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        Data Charts
+      </React.Fragment>
+    )
+  }
+}
+
+export default DataCharts;
---
+++ components/homepage/TwitterFeed.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+import styled from 'styled-components';
+
+class TwitterFeed extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        Twitter Feed
+      </React.Fragment>
+    )
+  }
+}
+
+export default TwitterFeed;
