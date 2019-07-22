562c2bc62f50cd92c8747eb04e67b4e78f9d22d3
Jason Zinn <jason@zinnfinity.com>
2019-07-22 13:39:27 -0500

    Loaded data into index component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Head from 'next/head';
 import styled from 'styled-components';
+import fetch from 'isomorphic-unfetch';
+import BarChart from '../components/charts/chartsjs/BarChart';
+import PieChart from '../components/charts/chartsjs/PieChart';
 import Primary from '../components/Primary';
 import Banner from '../components/homepage/Banner';
 import TwitterFeed from '../components/homepage/TwitterFeed';
@@ -11,7 +14,43 @@ import DataCharts from '../components/homepage/DataCharts';
 const pageTitle = 'Home Page';
 
 class Index extends React.Component {
+  static async getInitialProps() {
+    const res = await fetch('https://s3.amazonaws.com/tji-compressed-data/cdr_compressed.json');
+    const data = await res.json();
+    return { data };
+  }
+
+  state = {
+    age_at_time_of_death: this.props.data.meta.lookups.age_at_time_of_death,
+    agency_name: this.props.data.meta.lookups.agency_name,
+    death_location_county: this.props.data.meta.lookups.death_location_county,
+    death_location_type: this.props.data.meta.lookups.death_location_type,
+    manner_of_death: this.props.data.meta.lookups.manner_of_death,
+    means_of_death: this.props.data.meta.lookups.means_of_death,
+    race: this.props.data.meta.lookups.race,
+    sex: this.props.data.meta.lookups.sex,
+    type_of_custody: this.props.data.meta.lookups.type_of_custody,
+    year: this.props.data.meta.lookups.year,
+    currentData: this.props.data.records,
+    recordCount: this.props.data.meta.num_records,
+  };
+
   render() {
+    const { meta } = this.props.data;
+    const {
+      age_at_time_of_death,
+      agency_name,
+      death_location_county,
+      death_location_type,
+      manner_of_death,
+      means_of_death,
+      race,
+      sex,
+      type_of_custody,
+      year,
+    } = meta.lookups;
+    console.log(meta.num_records);
+
     return (
       <React.Fragment>
         <Head>
