2e41837f5836842ad4149a7ae5eaa933af5844a7
Jason Zinn <jason@zinnfinity.com>
2019-07-22 14:24:33 -0500

    Set bar chart background color

--- components/charts/chartsjs/BarChart.js
+++ components/charts/chartsjs/BarChart.js
@@ -13,12 +13,10 @@ const calculateData = (title, meta, metaData) => {
     datasets: [
       {
         label: title,
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
-        borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
         data: deathsByDataType,
+        fill: false,
+        backgroundColor: '#0B5D93',
+        lineTension: 0.1,
       },
     ],
   };
--- theme.js
+++ theme.js
@@ -29,78 +29,72 @@ export default {
 
   // Text
   siteTitle: 'Texas Justice Initiative',
+
+// Chart Color Palette
+
+  charts: {
+    colors: {
+      redHue1: '#FF8F8F', /* lighest red */
+      redHue2: '#F95858', /* lighter red */
+      redHue3: '#CE2727', /* light red */
+      redHue4: '#AA1111', /* red */
+      redHue5: '#721616', /* dark red */
+      redHue6: '#490B0B', /* darkest red */
+      
+      blueHue1: '#83E5FF', /* lighest blue */
+      blueHue2: '#64B8DD', /* lighter blue */
+      blueHue3: '#348CB2', /* light blue */
+      blueHue4: '#0B5D93', /* blue */
+      blueHue5: '#04405B', /* dark blue */
+      blueHue6: '#052C42', /* darkest blue */
+      
+      yellowHue1: '#FFFD00', /* lighest yellow */
+      yellowHue2: '#FFD400', /* lighter yellow */
+      yellowHue3: '#FFBC00', /* light yellow */
+      yellowHue4: '#E2A203', /* yellow */
+      yellowHue5: '#BC9800', /* dark yellow */
+      yellowHue6: '#A57F08', /* darkest yellow */
+    }
+  }
 };
 
 // ###### Variables from v1 #######
 
-/* colors */
-
-// $tji-blue-darker: #00436f;
-// $tji-blue-light: #6bafdc;
-// $tji-blue-lighter: #ccecf7;
-// $tji-color-white: #FFFFFF;
-// $tji-color-gray-lightest: #F4F4F4;
-// $tji-color-gray-lighter: #EAEAEA;
-// $tji-color-gray-light: #CBCBCB;
-// $tji-color-gray: #919191;
-// $tji-color-gray-dark: #363636;
-
-// $tji-color-red-hue-1: '#FF8F8F'; /* lighest red */
-// $tji-color-red-hue-2: '#F95858'; /* lighter red */
-// $tji-color-red-hue-3: '#CE2727'; /* light red */
-// $tji-color-red-hue-4: '#AA1111'; /* red */
-// $tji-color-red-hue-5: '#721616'; /* dark red */
-// $tji-color-red-hue-6: '#490B0B'; /* darkest red */
-
-// $tji-color-blue-hue-1: '#83E5FF'; /* lighest blue */
-// $tji-color-blue-hue-2: '#64B8DD'; /* lighter blue */
-// $tji-color-blue-hue-3: '#348CB2'; /* light blue */
-// $tji-color-blue-hue-4: '#0B5D93'; /* blue */
-// $tji-color-blue-hue-5: '#04405B'; /* dark blue */
-// $tji-color-blue-hue-6: '#052C42'; /* darkest blue */
-
-// $tji-color-yellow-hue-1: '#FFFD00'; /* lighest yellow */
-// $tji-color-yellow-hue-2: '#FFD400'; /* lighter yellow */
-// $tji-color-yellow-hue-3: '#FFBC00'; /* light yellow */
-// $tji-color-yellow-hue-4: '#E2A203'; /* yellow */
-// $tji-color-yellow-hue-5: '#BC9800'; /* dark yellow */
-// $tji-color-yellow-hue-6: '#A57F08'; /* darkest yellow */
-
 // /* fonts */
-// $headingFont: 'museo', Arial, sans-serif;
-// $bodyFont: 'Raleway', sans-serif; //400, 700 weight
+// $headingFont: 'museo', Arial, sans-serif,
+// $bodyFont: 'Raleway', sans-serif, //400, 700 weight
 
 // /* spacing */
-// $tji-spacing-xxs: .25rem;
-// $tji-spacing-xs: .5rem;
-// $tji-spacing-sm: 1rem;
-// $tji-spacing-md: 1.5rem;
-// $tji-spacing-lg: 2rem;
-// $tji-spacing-xl: 3rem;
+// $tji-spacing-xxs: .25rem,
+// $tji-spacing-xs: .5rem,
+// $tji-spacing-sm: 1rem,
+// $tji-spacing-md: 1.5rem,
+// $tji-spacing-lg: 2rem,
+// $tji-spacing-xl: 3rem,
 
 // /* typography */
-// $tji-font-size-xs: 0.8rem;
-// $tji-font-size-sm: 1rem;
-// $tji-font-size-md: 1.7rem;
-// $tji-font-size-lg: 2.6rem;
-// $tji-font-size-xl: 3.6rem;
+// $tji-font-size-xs: 0.8rem,
+// $tji-font-size-sm: 1rem,
+// $tji-font-size-md: 1.7rem,
+// $tji-font-size-lg: 2.6rem,
+// $tji-font-size-xl: 3.6rem,
 
-// $tji-line-height-xs: 1.0rem;
-// $tji-line-height-sm: 2.4rem;
-// $tji-line-height-md: 2.7rem;
-// $tji-line-height-lg: 3.6rem;
-// $tji-line-height-xl: 4.6rem;
+// $tji-line-height-xs: 1.0rem,
+// $tji-line-height-sm: 2.4rem,
+// $tji-line-height-md: 2.7rem,
+// $tji-line-height-lg: 3.6rem,
+// $tji-line-height-xl: 4.6rem,
 
-// $font__weight-normal: 400;
-// $font__weight-bold: 700;
+// $font__weight-normal: 400,
+// $font__weight-bold: 700,
 
 // /* dividers */
-// $tji-border: 1px solid $tji-color-gray-light;
+// $tji-border: 1px solid $tji-color-gray-light,
 
 // /* break-points (to match flexbox grid breakpoints) */
-// $tji-break-point-sm: 48rem;
-// $tji-break-point-md: 64rem;
-// $tji-break-point-lg: 75rem;
+// $tji-break-point-sm: 48rem,
+// $tji-break-point-md: 64rem,
+// $tji-break-point-lg: 75rem,
 
 // /* charts */
-// $tji-chart-legend-size: 1rem;
+// $tji-chart-legend-size: 1rem,
