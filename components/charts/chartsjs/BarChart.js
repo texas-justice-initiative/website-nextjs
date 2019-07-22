ffbf48e7087eb563ffa1201f02bd6358698b1b10
Jason Zinn <jason@zinnfinity.com>
2019-07-22 15:04:21 -0500

    Moved chart color palette into BarChart.js

--- components/charts/chartsjs/BarChart.js
+++ components/charts/chartsjs/BarChart.js
@@ -25,6 +25,32 @@ const calculateData = (title, meta, metaData) => {
 const DeathsByDataType = props => {
   const { title, meta, metaData } = props;
   const data = calculateData(title, meta, metaData);
+
+  // Chart Color Palette
+
+  const colors = {
+    redHue1: '#FF8F8F', /* lighest red */
+    redHue2: '#F95858', /* lighter red */
+    redHue3: '#CE2727', /* light red */
+    redHue4: '#AA1111', /* red */
+    redHue5: '#721616', /* dark red */
+    redHue6: '#490B0B', /* darkest red */
+    
+    blueHue1: '#83E5FF', /* lighest blue */
+    blueHue2: '#64B8DD', /* lighter blue */
+    blueHue3: '#348CB2', /* light blue */
+    blueHue4: '#0B5D93', /* blue */
+    blueHue5: '#04405B', /* dark blue */
+    blueHue6: '#052C42', /* darkest blue */
+    
+    yellowHue1: '#FFFD00', /* lighest yellow */
+    yellowHue2: '#FFD400', /* lighter yellow */
+    yellowHue3: '#FFBC00', /* light yellow */
+    yellowHue4: '#E2A203', /* yellow */
+    yellowHue5: '#BC9800', /* dark yellow */
+    yellowHue6: '#A57F08', /* darkest yellow */
+  };
+
   return (
     <React.Fragment>
       <h2>{title}</h2>
