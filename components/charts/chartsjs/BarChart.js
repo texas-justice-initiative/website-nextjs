27596a5536c90ae8b6fd8b2f688f2350f7f18ea5
Jason Zinn <jason@zinnfinity.com>
2019-07-23 15:50:29 -0500

    Updated BarChart.js with new options setup

--- components/charts/chartsjs/BarChart.js
+++ components/charts/chartsjs/BarChart.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Bar } from 'react-chartjs-2';
+import { ChartLabels } from 'chartjs-plugin-labels';
 
 const calculateData = (title, meta, metaData) => {
   const filterItems = (arr, query) => arr.filter(meta => meta === query);
@@ -22,41 +24,67 @@ const calculateData = (title, meta, metaData) => {
   };
 };
 
+// Chart Color Palette
+
+const colors = {
+  redHue1: '#FF8F8F', /* lighest red */
+  redHue2: '#F95858', /* lighter red */
+  redHue3: '#CE2727', /* light red */
+  redHue4: '#AA1111', /* red */
+  redHue5: '#721616', /* dark red */
+  redHue6: '#490B0B', /* darkest red */
+
+  blueHue1: '#83E5FF', /* lighest blue */
+  blueHue2: '#64B8DD', /* lighter blue */
+  blueHue3: '#348CB2', /* light blue */
+  blueHue4: '#0B5D93', /* blue */
+  blueHue5: '#04405B', /* dark blue */
+  blueHue6: '#052C42', /* darkest blue */
+
+  yellowHue1: '#FFFD00', /* lighest yellow */
+  yellowHue2: '#FFD400', /* lighter yellow */
+  yellowHue3: '#FFBC00', /* light yellow */
+  yellowHue4: '#E2A203', /* yellow */
+  yellowHue5: '#BC9800', /* dark yellow */
+  yellowHue6: '#A57F08', /* darkest yellow */
+};
+
+const options = {
+  maintainAspectRatio: true,
+  title: {
+    display: false,
+  },
+  legend: {
+    display: false
+  },
+  scales: {
+    yAxes: [{
+      ticks: {
+        suggestedMax: 900,
+        min: 0,
+      }
+    }]
+  },
+  layout: {
+    padding: 20
+  }
+};
+
 const DeathsByDataType = props => {
   const { title, meta, metaData } = props;
   const data = calculateData(title, meta, metaData);
 
-  // Chart Color Palette
-
-  const colors = {
-    redHue1: '#FF8F8F', /* lighest red */
-    redHue2: '#F95858', /* lighter red */
-    redHue3: '#CE2727', /* light red */
-    redHue4: '#AA1111', /* red */
-    redHue5: '#721616', /* dark red */
-    redHue6: '#490B0B', /* darkest red */
-    
-    blueHue1: '#83E5FF', /* lighest blue */
-    blueHue2: '#64B8DD', /* lighter blue */
-    blueHue3: '#348CB2', /* light blue */
-    blueHue4: '#0B5D93', /* blue */
-    blueHue5: '#04405B', /* dark blue */
-    blueHue6: '#052C42', /* darkest blue */
-    
-    yellowHue1: '#FFFD00', /* lighest yellow */
-    yellowHue2: '#FFD400', /* lighter yellow */
-    yellowHue3: '#FFBC00', /* light yellow */
-    yellowHue4: '#E2A203', /* yellow */
-    yellowHue5: '#BC9800', /* dark yellow */
-    yellowHue6: '#A57F08', /* darkest yellow */
-  };
-
   return (
-    <React.Fragment>
-      <h2>{title}</h2>
-      <Bar data={data} />
-    </React.Fragment>
+    <div>
+      <ChartTitle>{title}</ChartTitle>
+      <Bar data={data} options={options} />
+    </div>
   );
 };
 
 export default DeathsByDataType;
+
+const ChartTitle = styled.h3`
+  color: ${props => props.theme.colors.black};
+  text-align: center;
+`;
