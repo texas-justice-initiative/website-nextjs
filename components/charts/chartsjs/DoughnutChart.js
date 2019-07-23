4fd8cf3a0b4d340a27cba06fd1669d9ee4451f0f
Jason Zinn <jason@zinnfinity.com>
2019-07-23 10:18:29 -0500

    Added color palette to Doughnut chart

--- components/charts/chartsjs/DoughnutChart.js
+++ components/charts/chartsjs/DoughnutChart.js
@@ -2,6 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 import { Doughnut } from 'react-chartjs-2';
 
+// Chart Color Palette
+const colors = {
+  redHue1: '#FF8F8F', /* lighest red */
+  redHue2: '#F95858', /* lighter red */
+  redHue3: '#CE2727', /* light red */
+  redHue4: '#AA1111', /* red */
+  redHue5: '#721616', /* dark red */
+  redHue6: '#490B0B', /* darkest red */
+
+  blueHue1: '#83E5FF', /* lighest blue */
+  blueHue2: '#64B8DD', /* lighter blue */
+  blueHue3: '#348CB2', /* light blue */
+  blueHue4: '#0B5D93', /* blue */
+  blueHue5: '#04405B', /* dark blue */
+  blueHue6: '#052C42', /* darkest blue */
+
+  yellowHue1: '#FFFD00', /* lighest yellow */
+  yellowHue2: '#FFD400', /* lighter yellow */
+  yellowHue3: '#FFBC00', /* light yellow */
+  yellowHue4: '#E2A203', /* yellow */
+  yellowHue5: '#BC9800', /* dark yellow */
+  yellowHue6: '#A57F08', /* darkest yellow */
+};
+
 const data = {
   labels: ['Red', 'Green', 'Yellow'],
   datasets: [
