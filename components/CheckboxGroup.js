fbbfe717dd990f063b3abe985b3b6cd9ff985d25
Jason Zinn <jason@zinnfinity.com>
2019-07-24 14:23:00 -0500

    Added general checkbox group styles

--- components/CheckboxGroup.js
+++ components/CheckboxGroup.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const CheckboxGroup = ({ name, values, handler }) => (
-  <fieldset>
-    <legend>{name}</legend>
+  <Fieldset>
+    <legend>
+      {name} <span className="checkbox-group__toggle">&#9660;</span>
+    </legend>
     {values.map(value => (
       <div>
         <input onChange={handler} id={value} type="checkbox" name={name} defaultChecked="checked" value={value} />
         <label htmlFor={value}>{isNaN(value) ? value.toLowerCase() : value}</label>
       </div>
     ))}
-  </fieldset>
+  </Fieldset>
 );
 
 export default CheckboxGroup;
+
+const Fieldset = styled.fieldset`
+  margin: 2rem 0;
+  padding: 0.35em 0.75em 0.625em;
+
+  legend {
+    cursor: pointer;
+    padding: 0 0.5rem;
+    font-weight: 800;
+    text-transform: capitalize;
+    white-space: normal;
+  }
+`;
