9dc09b988dfada0d97d0cd66bb03df2c9b4ce34d
Jason Zinn <jason@zinnfinity.com>
2019-07-24 14:49:17 -0500

    Added toggle function to checkbox groups

--- components/CheckboxGroup.js
+++ components/CheckboxGroup.js
@@ -18,14 +18,16 @@ class CheckboxGroup extends React.Component {
   render() {
     const { name, values, handler } = this.props;
     return (
-      <Fieldset className={this.state.collapsed}>
+      <Fieldset>
         <legend onClick={this.toggleCheckboxGroup}>
           {name.replace(/_/g, ' ')} <span className="checkbox-group__toggle">&#9660;</span>
         </legend>
         {values.map(value => (
-          <div>
-            <input onChange={handler} id={value} type="checkbox" name={name} defaultChecked="checked" value={value} />
-            <label htmlFor={value}>{isNaN(value) ? value.toLowerCase() : value}</label>
+          <div className={!this.state.collapsed ? 'open' : 'closed'}>
+            <div>
+              <input onChange={handler} id={value} type="checkbox" name={name} defaultChecked="checked" value={value} />
+              <label htmlFor={value}>{isNaN(value) ? value.toLowerCase() : value}</label>
+            </div>
           </div>
         ))}
       </Fieldset>
@@ -38,6 +40,26 @@ export default CheckboxGroup;
 const Fieldset = styled.fieldset`
   margin: 2rem 0;
   padding: 0.35em 0.75em 0.625em;
+
+  > div {
+
+    &.open {
+      max-height: 500px;
+      overflow-y: hidden;
+
+      > div {
+        opacity: 1;
+      }
+    }
+
+    &.closed {
+      max-height: 0;
+
+      > div {
+        opacity: 0;
+      }
+    }
+  }
 
   legend {
     cursor: pointer;
