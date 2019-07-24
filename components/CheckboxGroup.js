15195fc3fd3c64e0c93f33946111418f6d049b56
Jason Zinn <jason@zinnfinity.com>
2019-07-24 14:38:00 -0500

    Made Checkbox component stateful

--- components/CheckboxGroup.js
+++ components/CheckboxGroup.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CheckboxGroup = ({ name, values, handler }) => (
-  <Fieldset>
-    <legend>
-      {name} <span className="checkbox-group__toggle">&#9660;</span>
-    </legend>
-    {values.map(value => (
-      <div>
-        <input onChange={handler} id={value} type="checkbox" name={name} defaultChecked="checked" value={value} />
-        <label htmlFor={value}>{isNaN(value) ? value.toLowerCase() : value}</label>
-      </div>
-    ))}
-  </Fieldset>
-);
+class CheckboxGroup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { collapsed: false };
+  }
+
+  render() {
+    const { name, values, handler } = this.props;
+    return (
+      <Fieldset className={this.state.collapsed}>
+        <legend>
+          {name.replace(/_/g, ' ')} <span className="checkbox-group__toggle">&#9660;</span>
+        </legend>
+        {values.map(value => (
+          <div>
+            <input onChange={handler} id={value} type="checkbox" name={name} defaultChecked="checked" value={value} />
+            <label htmlFor={value}>{isNaN(value) ? value.toLowerCase() : value}</label>
+          </div>
+        ))}
+      </Fieldset>
+    );
+  }
+}
 
 export default CheckboxGroup;
 
