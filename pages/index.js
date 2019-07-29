86a78e29d76d6c78715466cbb7ba2a80ebf59d97
Jason Zinn <jason@zinnfinity.com>
2019-07-29 10:34:12 -0500

    update dataset variable name to be specific

--- pages/index.js
+++ pages/index.js
@@ -14,28 +14,30 @@ const pageTitle = 'Home Page';
 class Index extends React.Component {
   static async getInitialProps() {
     const res = await fetch('https://s3.amazonaws.com/tji-compressed-data/cdr_compressed.json');
-    const data = await res.json();
-    return { data };
+    const custodialDeaths = await res.json();
+    return { custodialDeaths };
   }
 
   state = {
-    age_at_time_of_death: this.props.data.meta.lookups.age_at_time_of_death,
-    agency_name: this.props.data.meta.lookups.agency_name,
-    death_location_county: this.props.data.meta.lookups.death_location_county,
-    death_location_type: this.props.data.meta.lookups.death_location_type,
-    manner_of_death: this.props.data.meta.lookups.manner_of_death,
-    means_of_death: this.props.data.meta.lookups.means_of_death,
-    race: this.props.data.meta.lookups.race,
-    sex: this.props.data.meta.lookups.sex,
-    type_of_custody: this.props.data.meta.lookups.type_of_custody,
-    year: this.props.data.meta.lookups.year,
-    currentData: this.props.data.records,
-    recordCount: this.props.data.meta.num_records,
+    custodialDeaths: {
+      age_at_time_of_death: this.props.custodialDeaths.meta.lookups.age_at_time_of_death,
+      agency_name: this.props.custodialDeaths.meta.lookups.agency_name,
+      death_location_county: this.props.custodialDeaths.meta.lookups.death_location_county,
+      death_location_type: this.props.custodialDeaths.meta.lookups.death_location_type,
+      manner_of_death: this.props.custodialDeaths.meta.lookups.manner_of_death,
+      means_of_death: this.props.custodialDeaths.meta.lookups.means_of_death,
+      race: this.props.custodialDeaths.meta.lookups.race,
+      sex: this.props.custodialDeaths.meta.lookups.sex,
+      type_of_custody: this.props.custodialDeaths.meta.lookups.type_of_custody,
+      year: this.props.custodialDeaths.meta.lookups.year,
+      currentData: this.props.custodialDeaths.records,
+      recordCount: this.props.custodialDeaths.meta.num_records,
+    }
   };
 
   render() {
-    const { data } = this.props;
-    const { meta } = this.props.data;
+    const { custodialDeaths } = this.props;
+    const { meta } = this.props.custodialDeaths;
     const {
       age_at_time_of_death,
       agency_name,
@@ -56,11 +58,11 @@ class Index extends React.Component {
         </Head>
         <Primary fullWidth="true">
           <FlexWrap>
-            <Banner numDeaths={meta.num_records} year={year} yearData={this.state.currentData.year}></Banner>
+            <Banner numDeaths={meta.num_records} year={year} yearData={this.state.custodialDeaths.currentData.year}></Banner>
             <TwitterFeed />
             <NewsFeed />
             <StateofData />
-            <DataCharts data={data} />
+            <DataCharts data={custodialDeaths} />
           </FlexWrap>
         </Primary>
       </React.Fragment>
