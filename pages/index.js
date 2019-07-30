394468bcfa3e2fe1e9b6a64bee9791d2b0e74c21
Jason Zinn <jason@zinnfinity.com>
2019-07-30 07:53:59 -0500

    Removed components we no longer want

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,8 @@ import styled from 'styled-components';
 import fetch from 'isomorphic-unfetch';
 import Primary from '../components/Primary';
 import Banner from '../components/homepage/Banner';
-import TwitterFeed from '../components/homepage/TwitterFeed';
 import NewsFeed from '../components/homepage/NewsFeed';
 import StateofData from '../components/homepage/StateofData';
-import DataCharts from '../components/homepage/DataCharts';
 import Datasets from '../data/datasets';
 
 const pageTitle = 'Home Page';
@@ -15,6 +13,7 @@ const pageTitle = 'Home Page';
 class Index extends React.Component {
   state = {
     isLoading: true,
+    datasets: [],
     data: [],
     error: null,
   };
@@ -26,16 +25,15 @@ class Index extends React.Component {
 
   // Loop through our dataset array and fetch each dataset
   fetchData(datasets) {
-
     // Count our dataset length so we can set isLoading to false once ALL datasets have loaded
     let counter = datasets.length;
     datasets.forEach(dataset => {
       fetch(dataset.url)
         .then(response => response.json())
         .then(data => {
-
           // Add each dataset into state
           this.setState({
+            datasets: [...this.state.datasets, dataset.slug],
             data: [...this.state.data, data],
           });
 
@@ -43,7 +41,7 @@ class Index extends React.Component {
           counter -= 1;
 
           // If we finish loading the last dataset, set isLoading to false
-          if (counter === 0 ) {
+          if (counter === 0) {
             this.setState({
               isLoading: false,
             });
@@ -55,26 +53,13 @@ class Index extends React.Component {
 
   render() {
     let meta;
-    const { isLoading, data, error } = this.state;
+    const { isLoading, datasets, data, error } = this.state;
 
     // If we have loaded all of our data, setup our initial chart
     if (isLoading === false) {
-      const custodialDeaths = data[2];
+      const custodialDeaths = data[datasets.indexOf('custodialDeaths')];
       const { meta } = custodialDeaths;
       const { lookups } = meta;
-      const {
-        age_at_time_of_death,
-        agency_name,
-        death_location_county,
-        death_location_type,
-        manner_of_death,
-        means_of_death,
-        race,
-        sex,
-        type_of_custody,
-        year,
-      } = meta.lookups;
-      console.log(meta, year, custodialDeaths);
 
       return (
         <React.Fragment>
@@ -83,29 +68,24 @@ class Index extends React.Component {
           </Head>
           <Primary fullWidth="true">
             <FlexWrap>
-              <Banner numDeaths={meta.num_records} year={year} yearData={custodialDeaths.records.year}></Banner>
-              <TwitterFeed />
+              <Banner numDeaths={meta.num_records} year={meta.lookups.year} yearData={custodialDeaths.records.year} />
               <NewsFeed />
               <StateofData />
-              <DataCharts data={custodialDeaths} />
-            </FlexWrap>
-          </Primary>
-        </React.Fragment>
-      )
-    } else {
-      return (
-        <React.Fragment>
-          <Head>
-            <title>Texas Justice Initiative | {pageTitle}</title>
-          </Head>
-          <Primary fullWidth="true">
-            <FlexWrap>
-              Still loading
             </FlexWrap>
           </Primary>
         </React.Fragment>
       );
     }
+
+    // Return loading screen
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Texas Justice Initiative | {pageTitle}</title>
+        </Head>
+        <Primary fullWidth="true">Loading...</Primary>
+      </React.Fragment>
+    );
   }
 }
 
