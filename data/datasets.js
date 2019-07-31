9189c64b9289c98c0c3b53ff1d00402d4dbbd2fd
Jason Zinn <jason@zinnfinity.com>
2019-07-31 09:55:44 -0500

    Added styling to change chart buttons

--- data/datasets.js
+++ data/datasets.js
@@ -3,16 +3,19 @@ export default [
   {
     slug: 'custodialDeaths',
     name: 'Custodial Deaths',
+    icon: 'custodial_deaths.svg', // File name for the icon kept in /images/
     url: 'https://s3.amazonaws.com/tji-compressed-data/cdr_compressed.json',
   },
   {
     slug: 'civiliansShot',
     name: 'Civilians Shot by Officers',
+    icon: 'civilians_shot.svg', // File name for the icon kept in /images/
     url: 'https://s3.amazonaws.com/tji-compressed-data/ois_compressed.json',
   },
   {
     slug: 'officersShot',
     name: 'Officers Shot by Civilians',
+    icon: 'officers_shot.svg', // File name for the icon kept in /images/
     url: 'https://s3.amazonaws.com/tji-compressed-data/ois_officers_compressed.json',
   },
 ];
