7e390f2fa63c108aed48cc44a002960f3ff0945a
Jason Zinn <jason@zinnfinity.com>
2019-07-31 10:54:46 -0500

    Added Chart title to datasets json

--- data/datasets.js
+++ data/datasets.js
@@ -3,18 +3,21 @@ export default [
   {
     slug: 'custodialDeaths',
     name: 'Custodial Deaths',
+    chartTitle: 'Deaths in Custody since 2005',
     icon: 'custodial_deaths.svg', // File name for the icon kept in /images/
     url: 'https://s3.amazonaws.com/tji-compressed-data/cdr_compressed.json',
   },
   {
     slug: 'civiliansShot',
     name: 'Civilians Shot by Officers',
+    chartTitle: 'Civilians Shot by Officers since 2015',
     icon: 'civilians_shot.svg', // File name for the icon kept in /images/
     url: 'https://s3.amazonaws.com/tji-compressed-data/ois_compressed.json',
   },
   {
     slug: 'officersShot',
     name: 'Officers Shot by Civilians',
+    chartTitle: 'Officers Shot by Civilians since 2015',
     icon: 'officers_shot.svg', // File name for the icon kept in /images/
     url: 'https://s3.amazonaws.com/tji-compressed-data/ois_officers_compressed.json',
   },
