139b25fddc63d90712ac6727eef5b14830e480d4
Jason Zinn <jason@zinnfinity.com>
2019-08-21 14:35:33 -0500

    Removed MailChimp import

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
-import MailChimpForm from './MailChimpForm';
 
 class Footer extends Component {
   render() {
