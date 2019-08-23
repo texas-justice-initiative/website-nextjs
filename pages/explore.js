05152b32c409e3f9986ddd5dac65affabcd8238d
Jason Zinn <jason@zinnfinity.com>
2019-08-23 10:28:21 -0500

    import filter panel

--- pages/explore.js
+++ pages/explore.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
+import FilterPanel from '../components/FilterPanel';
 import fetch from 'isomorphic-unfetch';
 import datasets from '../data/datasets_test';
 
