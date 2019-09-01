ba23d8bb8abeb50e8ca22a11dbe3a723ed8cfc1b
Nick Holden <nick.r.holden@gmail.com>
2019-09-01 14:06:24 -0700

    Extract FilterContainer component

--- components/explore-the-data-page/CheckboxGroup.js
+++ components/explore-the-data-page/CheckboxGroup.js
@@ -1,29 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
 
 class CheckboxGroup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { collapsed: false };
-
-    this.toggleCheckboxGroup = this.toggleCheckboxGroup.bind(this);
-  }
-
-  toggleCheckboxGroup() {
-    this.setState(state => ({
-      collapsed: !state.collapsed,
-    }));
-  }
-
   render() {
     const { name, values, handler, isChecked } = this.props;
     return (
-      <Fieldset>
-        <legend onClick={this.toggleCheckboxGroup} className={!this.state.collapsed ? 'open' : 'closed'}>
-          {name.replace(/_/g, ' ')} <span className="checkbox-group__toggle">&#9660;</span>
-        </legend>
+      <div>
         {values.map(value => (
-          <div key={value} className={!this.state.collapsed ? 'open' : 'closed'}>
+          <div key={value}>
             <label htmlFor={value}>
               <input
                 onChange={handler}
@@ -37,50 +20,9 @@ class CheckboxGroup extends React.Component {
             </label>
           </div>
         ))}
-      </Fieldset>
+      </div>
     );
   }
 }
 
 export default CheckboxGroup;
-
-const Fieldset = styled.fieldset`
-  margin: 2rem 4rem;
-  padding: 0.35em 0.75em 0.625em;
-
-  > div {
-    &.open {
-      max-height: 500px;
-      overflow-y: hidden;
-      opacity: 1
-    }
-
-    &.closed {
-      max-height: 0;
-      opacity: 0;
-    }
-  }
-
-  input {
-    margin-right: 0.5rem;
-  }
-
-  legend {
-    cursor: pointer;
-    padding: 0 0.5rem;
-    font-weight: 800;
-    text-transform: capitalize;
-    white-space: normal;
-
-    &.closed {
-      .checkbox-group__toggle {
-        transform: rotate(180deg);
-      }
-    }
-
-    .checkbox-group__toggle {
-      display: inline-block;
-      transition: transform 0.5s;
-    }
-  }
-`;
