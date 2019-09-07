5f0b704670e84c23c4f4f837403e098f399e4e6a
Nick Holden <nick.r.holden@gmail.com>
2019-09-07 13:37:29 -0700

    Fix test datasets

--- data/datasets_test.js
+++ data/datasets_test.js
@@ -26,7 +26,7 @@ export default {
     description: 'Shootings involving Texas law enforcement since Sept. 2015, as reported to the Office of the Attorney General.',
     icon: 'civilians_shot.svg', // File name for the icon kept in /images/
     urls: {
-      compressed: 'https://api.myjson.com/bins/uwkqf',
+      compressed: 'https://api.myjson.com/bins/1eoput',
       full: 'https://s3.us-east-2.amazonaws.com/tji-public-cleaned-datasets/shot_civilians.csv',
     },
     chart_configs: [
@@ -47,7 +47,7 @@ export default {
     description: 'Shootings that injured or killed Texas law enforcement officers since Sept. 2015, as reported to the Office of the Attorney General.',
     icon: 'officers_shot.svg', // File name for the icon kept in /images/
     urls: {
-      compressed: 'https://api.myjson.com/bins/19bhaf',
+      compressed: 'https://api.myjson.com/bins/13dhd1',
       full: 'https://s3.us-east-2.amazonaws.com/tji-public-cleaned-datasets/shot_officers.csv',
     },
     chart_configs: [
