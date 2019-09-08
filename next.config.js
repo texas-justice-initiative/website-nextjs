ad2231585c4b4fca50cf0dafe1181e41aa7e9955
Jen Udan <jen@lunarcollective.co>
2019-09-08 14:07:00 -0500

    Commented out target in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 const nextConfig = {
-  target: 'serverless',
+  // target: 'serverless',
   webpack: (config) => {
     config.module.rules.push(
       {
