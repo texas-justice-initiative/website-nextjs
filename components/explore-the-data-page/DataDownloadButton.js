4ebdf6f4102fd02515041b77a3860516990713af
Nick Holden <nick.r.holden@gmail.com>
2019-09-28 14:10:53 -0700

    Use dynamic props to make this more conventional React

--- components/explore-the-data-page/DataDownloadButton.js
+++ components/explore-the-data-page/DataDownloadButton.js
@@ -3,12 +3,6 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 class DataDownloadButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.downloadCsv = this.downloadCsv.bind(this);
-    this.hiddenLink = React.createRef();
-  }
-
   csvRows() {
     const { data } = this.props;
     const { records } = data;
@@ -27,25 +21,21 @@ class DataDownloadButton extends React.Component {
     return `data:text/csv;charset=utf-8,${csvBody}`;
   }
 
-  downloadCsv() {
-    const { fileName } = this.props;
-    const hiddenLink = this.hiddenLink.current;
-    hiddenLink.href = encodeURI(this.csvContent());
-    hiddenLink.download = fileName;
-    hiddenLink.click();
-  }
-
   render() {
-    const { children } = this.props;
+    const { children, fileName } = this.props;
 
     return (
       <div>
-        <Button className="btn btn--primary btn--chart-toggle" type="button" onClick={this.downloadCsv}>
+        <A
+          className="btn btn--primary btn--chart-toggle"
+          ref={this.hiddenLink}
+          href={encodeURI(this.csvContent())}
+          download={fileName}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           {children}
-        </Button>
-        <a ref={this.hiddenLink} target="_blank" style={{ display: 'none' }}>
-          {children}
-        </a>
+        </A>
       </div>
     );
   }
@@ -59,7 +49,7 @@ DataDownloadButton.propTypes = {
   children: PropTypes.string.isRequired,
 };
 
-const Button = styled.button`
+const A = styled.a`
   margin: 0 0 4rem 0;
   text-transform: none !important;
 `;
