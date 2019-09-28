21b171949df061682b9d958b158d0fc8376411ab
Nick Holden <nick.r.holden@gmail.com>
2019-09-28 13:37:13 -0700

    npm run lint:fix

--- next.config.js
+++ next.config.js
@@ -5,18 +5,16 @@ const optimizedImages = require('next-optimized-images');
 
 const nextConfig = {
   // target: 'serverless',
-  exportPathMap: function() {
+  exportPathMap() {
     return {
-      '/': { page: '/' }
-    }
+      '/': { page: '/' },
+    };
   },
-  webpack: (config) => {
-    config.module.rules.push(
-      {
-        test: /\.md$/,
-        use: 'frontmatter-markdown-loader'
-      }
-    )
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'frontmatter-markdown-loader',
+    });
     return config;
   },
 };
