7be7f77e58d3078aee60d26f437042aa16b3037e
Jason Zinn <jason@zinnfinity.com>
2019-10-04 16:20:39 -0500

    Add ChartNote component

---
+++ components/charts/chartsjs/ChartNote.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+function ChartNote(props) {
+  const { note } = props;
+
+  return <Note className="chart__plot-note">{note}</Note>;
+}
+
+export default ChartNote;
+
+ChartNote.propTypes = {
+  note: PropTypes.string,
+};
+
+const Note = styled.span`
+  display: block;
+  font-style: italic;
+  font-size: ${props => props.theme.sidebarFont__size};
+
+  &:before {
+    display: inline-block;
+    content: '';
+    height: 1.2rem;
+    width: 1.2rem;
+    margin-right: 0.5rem;
+    background: ${props => props.theme.colors.gray};
+  }
+`;
