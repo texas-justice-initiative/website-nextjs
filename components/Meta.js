aa9da8ebade3ac388bfe8e3cf6a586c714941bb4
Jason Zinn <jason@zinnfinity.com>
2019-10-11 10:57:02 -0500

    Added new favicon

--- components/Meta.js
+++ components/Meta.js
@@ -10,7 +10,7 @@ const Meta = props => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
-    <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.png" />
     <link href="https://fonts.googleapis.com/css?family=Domine:400,700|Roboto:400,700&display=swap" rel="stylesheet" />
     <title>{props => props.theme.siteTitle}</title>
   </Head>
