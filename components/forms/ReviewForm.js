f1477b8ad3cc5568133b83af0109b72f71678d00
Jason Zinn <jason@zinnfinity.com>
2019-10-20 13:55:05 -0500

    Removed paypal callback functions

--- components/forms/ReviewForm.js
+++ components/forms/ReviewForm.js
@@ -53,25 +53,6 @@ class ReviewForm extends React.Component {
       );
     }
 
-    const onSuccess = payment => {
-      // Congratulation, it came here means everything's fine!
-      console.log('The payment was succeeded!', payment);
-      // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
-    };
-
-    const onCancel = data => {
-      // User pressed "cancel" or close Paypal's popup!
-      console.log('The payment was cancelled!', data);
-      // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
-    };
-
-    const onError = err => {
-      // The main Paypal's script cannot be loaded or somethings block the loading of that script!
-      console.log('Error!', err);
-      // Because the Paypal's main script is loaded asynchronously from "https://www.paypalobjects.com/api/checkout.js"
-      // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
-    };
-
     return (
       <DonationReview>
         <header>
@@ -95,16 +76,7 @@ class ReviewForm extends React.Component {
             <b>Donation Amount:</b> ${total}
           </li>
         </ul>
-        <PaypalButton
-          env={env}
-          client={client}
-          commit
-          currency="USD"
-          total={total}
-          onSuccess={onSuccess}
-          onError={onError}
-          onCancel={onCancel}
-        />
+        <PaypalButton env={env} client={client} commit currency="USD" total={total} />
       </DonationReview>
     );
   }
