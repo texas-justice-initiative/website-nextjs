e94039b48e63edb051341082e5e546bb346126ff
Jason Zinn <jason@zinnfinity.com>
2019-10-23 12:25:42 -0500

    Moved pipeline graphic styles into component

--- components/images/PipelineImage.js
+++ components/images/PipelineImage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 export default function Image(props) {
   const { src, alt } = props;
   return (
-    <div className="tji-pipeline-graphic">
+    <PipeLineGraphic className="tji-pipeline-graphic">
       <img src={src} alt={alt} className="tji-pipeline-graphic__img" />
-    </div>
+    </PipeLineGraphic>
   );
 }
 
@@ -14,3 +15,21 @@ Image.propTypes = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string,
 };
+
+const PipeLineGraphic = styled.div`
+  text-align: center;
+  padding: 4rem 0;
+
+  .tji-pipeline-graphic__img {
+    width: 100%;
+    max-width: 600px;
+    height: auto;
+  }
+
+  /* Class to tag elements which should only show on mobile */
+  .mobile-only {
+    @media screen and (min-width: ${props => props.theme.medium}) {
+      display: none;
+    }
+  }
+`;
--- styles/GlobalStyle.js
+++ styles/GlobalStyle.js
@@ -163,24 +163,6 @@ const GlobalStyle = createGlobalStyle`
     width: 24px;
     margin-right: 1rem;
   }
-
-  /* Images */
-  .tji-pipeline-graphic {
-    text-align: center;
-    padding: 4rem 0;
-  }
-  .tji-pipeline-graphic__img {
-    width: 100%;
-    max-width: 600px;
-    height: auto;
-  }
-
-  /* Class to tag elements which should only show on mobile */
-  .mobile-only {
-    @media screen and (min-width: ${props => props.theme.medium}) {
-      display: none;
-    }
-  }
 `;
 
 export default GlobalStyle;
