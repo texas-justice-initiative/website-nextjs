ac4d916a7fe7bfb8b96bc55c8285a1de5e7bf13e
Michael Reed <1062477+haywood@users.noreply.github.com>
2019-11-06 08:25:30 -0600

    add note about html sanitization and markdown-it as explicit dep

--- components/Parser.js
+++ components/Parser.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import HTMLParser from 'html-react-parser';
 import sanitizeHtml from 'sanitize-html';
 
+// NOTE: sanitizeHtml strips img tags by default
+// See the default options at https://www.npmjs.com/package/sanitize-html#what-are-the-default-options
 const parseContent = content => HTMLParser(sanitizeHtml(content));
 
 const Parser = props => <React.Fragment>{parseContent(props.children)}</React.Fragment>;
