1f36fb77f1433fb4e831eca0ad33702dc7d0eb7c
Michael Reed <1062477+haywood@users.noreply.github.com>
2019-11-06 08:04:44 -0600

    Render markdown from descriptions

--- components/Parser.js
+++ components/Parser.js
@@ -3,8 +3,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import HTMLParser from 'html-react-parser';
+import sanitizeHtml from 'sanitize-html';
 
-const parseContent = content => HTMLParser(content);
+const parseContent = content => HTMLParser(sanitizeHtml(content));
 
 const Parser = props => <React.Fragment>{parseContent(props.children)}</React.Fragment>;
 
