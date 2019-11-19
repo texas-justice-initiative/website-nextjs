aecc16ea8c0b4ad98305c8613afb8ff6f65b3539
Jason Zinn <jason@zinnfinity.com>
2019-11-19 17:17:38 -0600

    Matched styles for page sidebars to homepage

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 const Sidebar = props => {
   const { children } = props;
-  return <StyledAside>{children}</StyledAside>;
+  return <StyledAside className="sidebar sidebar--subtle">{children}</StyledAside>;
 };
 
 export default Sidebar;
@@ -17,27 +17,8 @@ const StyledAside = styled.aside`
   flex: 1 0 342px;
   padding: 1em;
   width: 100%;
-  background-color: ${props => props.theme.colors.primaryBlue};
-  color: ${props => props.theme.colors.white};
-
-  h3 {
-    color: ${props => props.theme.colors.white};
-    border-bottom: 1px solid ${props => props.theme.colors.white};
-  }
-
-  a {
-    color: ${props => props.theme.colors.primaryYellow};
-  }
-
-  p {
-    font-size: ${props => props.theme.sidebarFont__size};
-    line-height: 1.25;
-  }
 
   @media screen and (min-width: ${props => props.theme.medium}) {
-    padding: 2em;
     width: 342px; /* props.theme.large / 3 */
-    box-shadow: -2px 0 3px rgba(65, 65, 65, 0.5);
-    min-height: calc(100vh - 100px);
   }
 `;
