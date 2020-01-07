e08508ca6a1572dc9a95c2523c1bba266401cb8b
Jason Zinn <jason@zinnfinity.com>
2020-01-07 09:59:21 -0600

    Added google analytics component to meta

--- components/Meta.js
+++ components/Meta.js
@@ -3,18 +3,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import { initGA, LogPageView } from './GoogleAnalytics';
 
 const siteTitle = 'Texas Justice Initiative';
 
-const Meta = props => (
-  <Head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta charSet="utf-8" />
-    <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.png" />
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,700&display=swap" rel="stylesheet" />
-    <title>{props => props.theme.siteTitle}</title>
-  </Head>
-);
+class Meta extends React.Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    LogPageView();
+  }
+
+  render() {
+    const { props } = this;
+
+    return (
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.png" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,700&display=swap" rel="stylesheet" />
+        <title>{props => props.theme.siteTitle}</title>
+      </Head>
+    );
+  }
+}
 
 export default Meta;
 
