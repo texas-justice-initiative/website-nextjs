da11f6249e47eb33f24dd622957cd37b16f51f98
Jason Zinn <jason@zinnfinity.com>
2020-01-08 10:28:53 -0600

    Updated pathname correctly

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -2,34 +2,26 @@ import React from 'react';
 import Router from 'next/router';
 import ReactGA from 'react-ga';
 
-/*
-async function fetchData(url) {
-  const res = await fetch(url);
-  const params = await res.json();
-  return params;
-}
-*/
-
 const initGA = () => {
   ReactGA.initialize('UA-119932656-2');
 };
 
-const LogPageView = () => {
-  console.log(window.location.pathname);
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
+const LogPageView = url => {
+  console.log(url);
+  ReactGA.set({ page: url });
+  ReactGA.pageview(url);
 };
 
 Router.events.on('routeChangeStart', url => {
-  console.log(`Loading: ${url}`);
   initGA();
-  LogPageView();
+  LogPageView(url);
 });
 
 export default class GoogleAnalytics extends React.Component {
   componentDidMount() {
+    const url = window.location.pathname;
     initGA();
-    LogPageView();
+    LogPageView(url);
   }
 
   render() {
