59548d1f9ac2cdc69a101295bf7b06d7fb804e75
Jason Zinn <jason@zinnfinity.com>
2021-01-17 08:46:02 -0600

    added test post

---
+++ pages/posts/[id].js
@@ -0,0 +1,32 @@
+// pages/posts/[id].js
+
+function Post({ post }) {
+    return (
+        <p>{post.id}</p>
+    )
+}
+
+// This function gets called at build time
+export async function getStaticPaths() {
+
+    // Get the paths we want to pre-render based on posts
+    const paths = {
+        params: { id: 1 },
+    };
+
+    // We'll pre-render only these paths at build time.
+    // { fallback: false } means other routes should 404.
+    return { paths, fallback: false }
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+    const post = {
+        id: params.id
+    }
+
+    // Pass post data to the page via props
+    return { props: { post } }
+}
+
+export default Post
