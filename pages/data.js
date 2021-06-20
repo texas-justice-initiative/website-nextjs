a11b01e5af757e1a6e1b148913ade73963871940
LayaTaal <jason@zinnfinity.com>
2021-06-20 15:51:33 -0500

    added field to netlify and output on interactive data page

--- pages/data.js
+++ pages/data.js
@@ -5,10 +5,15 @@ import Primary from '../components/Primary';
 import Hero from '../components/Hero';
 import DataTable from '../components/DataTable';
 import content from '../content/interactive.md';
+import Parser from '../components/Parser';
 
 const {
-  attributes: { title, description, datasets },
+  attributes: { title, description, usage, datasets },
 } = content;
+
+const md = require('markdown-it')({
+  html: true,
+});
 
 const Page = () => (
   <React.Fragment>
@@ -18,6 +23,7 @@ const Page = () => (
     <Layout fullWidth flexColumn>
       <Hero title={title} description={description} />
       <Primary>
+        <Parser>{md.render(usage)}</Parser>
         <DataTable datasets={datasets} />
       </Primary>
     </Layout>
