47d9b18c70bac8e9bc91b111e0f50fc0593fc6a2
Jason Zinn <jason@zinnfinity.com>
2021-06-21 18:21:36 -0500

    629/add license language (#636)
    * added field to netlify and output on interactive data page
    
    * Update Pages “interactive” (#634)
    
    * formatted terms
    
    * Update Pages “interactive” (#635)
    
    * updated layout to constrain table width
    
    * reordered imports

--- pages/data.js
+++ pages/data.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Head from 'next/head';
+import styled from 'styled-components';
 import Layout from '../components/Layout';
 import Primary from '../components/Primary';
 import Hero from '../components/Hero';
 import DataTable from '../components/DataTable';
 import content from '../content/interactive.md';
+import Parser from '../components/Parser';
 
 const {
-  attributes: { title, description, datasets },
+  attributes: { title, description, usage, datasets },
 } = content;
+
+const md = require('markdown-it')({
+  html: true,
+});
 
 const Page = () => (
   <React.Fragment>
@@ -17,10 +23,27 @@ const Page = () => (
     </Head>
     <Layout fullWidth flexColumn>
       <Hero title={title} description={description} />
+    </Layout>
+    <Layout>
       <Primary>
+        <Usage id="data-usage">
+          <Parser>{md.render(usage)}</Parser>
+        </Usage>
         <DataTable datasets={datasets} />
       </Primary>
     </Layout>
   </React.Fragment>
 );
 export default Page;
+
+const Usage = styled.div`
+  padding-bottom: 4rem;
+
+  @media (min-width: ${props => props.theme.medium}) {
+    padding-bottom: 0;
+  }
+
+  p {
+    margin: 0;
+  }
+`;
