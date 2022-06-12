509cbee7e8788282af1a4109a49c86cc484b9cd4
LayaTaal <jason@zinnfinity.com>
2022-06-12 18:43:34 -0400

    refactored blog page to use same functionality as topics page

--- pages/blog.js
+++ pages/blog.js
@@ -9,6 +9,132 @@ import BlogFilters from '../components/BlogFilters';
 import Paginate from '../components/Paginate';
 import Post from '../components/loop/Post';
 
+function filterPosts(posts, topics, authors) {
+  return posts.filter(post => {
+    let inTopic = false;
+    let hasAuthor = false;
+
+    if (topics.length !== 0) {
+      topics.forEach(topic => {
+        if (post.attributes.topics.indexOf(topic.attributes.title) !== -1) {
+          inTopic = true;
+        }
+      });
+    } else {
+      inTopic = true;
+    }
+
+    if (authors.length !== 0) {
+      authors.forEach(author => {
+        if (post.attributes.authors.indexOf(author.attributes.title) !== -1) {
+          hasAuthor = true;
+        }
+      });
+    } else {
+      hasAuthor = true;
+    }
+
+    return inTopic && hasAuthor;
+  });
+}
+
+export default function Blog({ posts, authors, topics }) {
+  const [filteredTopics, setFilteredTopics] = useState([]);
+  const [filteredAuthors, setFilteredAuthors] = useState([]);
+
+  const postsShown = filterPosts(posts, filteredTopics, filteredAuthors);
+
+  /**
+   * Handles updating active post array based upon selected authors
+   */
+  function handleSelectAuthors() {
+    const authorsFilters = document.querySelectorAll('.authors-filters__filter');
+
+    if (!authorsFilters) {
+      return;
+    }
+
+    const selectedAuthors = Array.prototype.slice
+      .call(authorsFilters)
+      .filter(author => author.checked === true)
+      .map(author => ({
+        attributes: {
+          title: author.name,
+        },
+      }));
+
+    if (selectedAuthors.length === 0) {
+      setFilteredAuthors([]);
+      return;
+    }
+
+    setFilteredAuthors(selectedAuthors);
+  }
+
+  /**
+   * Handles updating active post array based upon selected authors
+   */
+  function handleSelectTopics() {
+    const topicsFiters = document.querySelectorAll('.topics-filters__filter');
+
+    if (!topicsFiters) {
+      return;
+    }
+
+    const selectedTopics = Array.prototype.slice
+      .call(topicsFiters)
+      .filter(topic => topic.checked === true)
+      .map(topic => ({
+        attributes: {
+          title: topic.name,
+        },
+      }));
+
+    if (selectedTopics.length === 0) {
+      setFilteredTopics([]);
+      return;
+    }
+
+    setFilteredTopics(selectedTopics);
+  }
+
+  return (
+    <>
+      <NextSeo
+        title="Blog"
+        description="Blogs about our criminal justice data, publications, analyses, and code."
+        openGraph={{
+          description: 'Blogs about our criminal justice data, publications, analyses, and code.',
+        }}
+      />
+      <Layout>
+        <Primary fullWidth>
+          <h1>TJI Blog</h1>
+          <div>
+            <p>Where the TJI team posts blogs about our data, publications, analyses, and code.</p>
+          </div>
+
+          {postsShown && (
+            <Paginate basePath="/blog">
+              {postsShown.map(post => (
+                <Post post={post} />
+              ))}
+            </Paginate>
+          )}
+        </Primary>
+        <Sidebar>
+          <BlogFilters
+            authors={authors}
+            handleSelectAuthors={handleSelectAuthors}
+            topics={topics}
+            handleSelectTopics={handleSelectTopics}
+          />
+        </Sidebar>
+      </Layout>
+    </>
+  );
+}
+
 export async function getStaticProps() {
   const posts = (context => {
     const keys = context.keys();
@@ -72,113 +198,6 @@ export async function getStaticProps() {
     },
   };
 }
-
-function Blog({ posts, authors, topics }) {
-  const [postsShown, setPostsShown] = useState(posts);
-
-  /**
-   * Handles updating active post array based upon selected authors
-   */
-  function handleSelectAuthors() {
-    const authorsFilters = document.querySelectorAll('.authors-filters__filter');
-
-    if (!authorsFilters) {
-      return;
-    }
-
-    const selectedAuthors = Array.prototype.slice.call(authorsFilters).filter(author => author.checked === true);
-
-    if (selectedAuthors.length === 0) {
-      setPostsShown(posts);
-      return;
-    }
-
-    const filteredPosts = posts.filter(post => {
-      let postHasAuthor = false;
-
-      selectedAuthors.forEach(author => {
-        if (post.attributes.authors.indexOf(author.name) !== -1) {
-          postHasAuthor = true;
-        }
-      });
-
-      return postHasAuthor;
-    });
-
-    setPostsShown(filteredPosts);
-  }
-
-  /**
-   * Handles updating active post array based upon selected topics
-   * TODO: can be refactored with above handler to a single function
-   */
-  function handleSelectTopics() {
-    const topicsFilter = document.querySelectorAll('.topics-filters__filter');
-
-    if (!topicsFilter) {
-      return;
-    }
-
-    const selectedTopics = Array.prototype.slice.call(topicsFilter).filter(topic => topic.checked === true);
-
-    if (selectedTopics.length === 0) {
-      setPostsShown(posts);
-      return;
-    }
-
-    const filteredPosts = posts.filter(post => {
-      let postHasTopic = false;
-
-      selectedTopics.forEach(topic => {
-        if (post.attributes.topics.indexOf(topic.name) !== -1) {
-          postHasTopic = true;
-        }
-      });
-
-      return postHasTopic;
-    });
-
-    setPostsShown(filteredPosts);
-  }
-
-  return (
-    <>
-      <NextSeo
-        title="Blog"
-        description="Blogs about our criminal justice data, publications, analyses, and code."
-        openGraph={{
-          description: 'Blogs about our criminal justice data, publications, analyses, and code.',
-        }}
-      />
-      <Layout>
-        <Primary fullWidth>
-          <h1>TJI Blog</h1>
-          <div>
-            <p>Where the TJI team posts blogs about our data, publications, analyses, and code.</p>
-          </div>
-
-          {postsShown && (
-            <Paginate basePath="/blog">
-              {postsShown.map(post => (
-                <Post post={post} />
-              ))}
-            </Paginate>
-          )}
-        </Primary>
-        <Sidebar>
-          <BlogFilters
-            authors={authors}
-            handleSelectAuthors={handleSelectAuthors}
-            topics={topics}
-            handleSelectTopics={handleSelectTopics}
-          />
-        </Sidebar>
-      </Layout>
-    </>
-  );
-}
-
-export default Blog;
 
 Blog.propTypes = {
   posts: PropTypes.any,
