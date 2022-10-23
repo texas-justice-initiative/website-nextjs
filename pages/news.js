f8c83bde3e7c24580617fd820917c0030aaf33cb
alankrit kumar <alankrit.kumar.cse20@itbhu.ac.in>
2022-10-23 05:42:14 +0530

    Sorted News Articles (#837)
    * Added Searchbar in News page and added pagelinks component and restructures the data flow
    
    * Sorted news articles by date from latest to oldest
    
    * removed errros and commented code
    
    * Ran yarn lint and fixed the errors and issues, no more issue found
    
    * Capitalized Filename
    
    * Delete pagelinks.js

--- pages/news.js
+++ pages/news.js
@@ -1,57 +1,73 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+
 import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
+
 import Layout from '../components/Layout';
 import Primary from '../components/Primary';
 import Sidebar from '../components/Sidebar';
 import NewsFeed from '../components/NewsFeed';
 import content from '../content/newsfeed.md';
+import Pagelinks from '../components/Pagelinks';
 
-const {
+let {
   attributes: { title, news },
 } = content;
 
 const News = () => {
+  news = news.sort((a, b) => {
+    if (a.date > b.date) {
+      return -1;
+    }
+    if (a.date < b.date) {
+      return 1;
+    }
+    return 0;
+  });
   const perPage = 5;
-  const pageCount = Math.ceil(news.length / perPage);
-
   const router = useRouter();
   let { page } = router.query;
+  const [query, setQuery] = useState('');
   page = parseInt(page);
-  if (Number.isNaN(page) || page < 1 || page > pageCount) {
-    page = 1;
-  }
-
-  const pageLinks = [];
-  for (let pageNumber = 1; pageNumber <= pageCount; pageNumber += 1) {
-    if (pageNumber === page) {
-      pageLinks.push(
-        <PageNumber className="current" key={pageNumber}>
-          {pageNumber}
-        </PageNumber>
-      );
-    } else {
-      const pagePath = `/news?page=${pageNumber}`;
-
-      pageLinks.push(
-        <Link href={pagePath} key={pageNumber}>
-          <a href={pagePath} style={{ textDecoration: 'none' }}>
-            <PageNumber>{pageNumber}</PageNumber>
-          </a>
-        </Link>
-      );
-    }
-  }
-
   return (
     <React.Fragment>
       <NextSeo title={title} />
       <Layout>
         <Primary>
-          <NewsFeed page={page} perPage={perPage} />
-          <div style={{ textAlign: 'center' }}>{pageLinks}</div>
+          <input
+            type="text"
+            style={{ marginBottom: '1em' }}
+            placeholder="Search for relevant articles..."
+            onChange={event => {
+              setQuery(event.target.value);
+            }}
+          />
+          <NewsFeed
+            page={page}
+            perPage={perPage}
+            // eslint-disable-next-line array-callback-return
+            news={news.filter(val => {
+              if (query === '') {
+                return val;
+              }
+              if (val.title.toLowerCase().includes(query.toLowerCase())) {
+                return val;
+              }
+            })}
+          />
+          <Pagelinks
+            page={page}
+            perPage={perPage}
+            // eslint-disable-next-line array-callback-return
+            news={news.filter(val => {
+              if (query === '') {
+                return val;
+              }
+              if (val.title.toLowerCase().includes(query.toLowerCase())) {
+                return val;
+              }
+            })}
+          />
         </Primary>
         <Sidebar />
       </Layout>
@@ -59,18 +75,3 @@ const News = () => {
   );
 };
 export default News;
-
-const PageNumber = styled.span`
-  padding: 0.5em 0.8em;
-  border: 1px solid ${props => props.theme.colors.grayLight};
-  margin-left: -1px;
-  color: ${props => props.theme.colors.primaryBlue};
-  background-color: ${props => props.theme.colors.white};
-  transition: all 0.35s;
-
-  &.current,
-  &:hover {
-    color: ${props => props.theme.colors.white};
-    background-color: ${props => props.theme.colors.primaryBlue};
-  }
-`;
