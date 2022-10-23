0511ae00761c019676584edfb22f08ff6ba3dc2c
LayaTaal <jason@zinnfinity.com>
2022-10-22 20:30:27 -0400

    added reports schema

---
+++ schema/tcjs-reports.js
@@ -0,0 +1,18 @@
+export default {
+  immigration: {
+    slug: 'immigration',
+    label: 'Immigration',
+  },
+  jailPopulation: {
+    slug: 'jail_population',
+    label: 'Jail Population',
+  },
+  pregnancies: {
+    slug: 'pregnancies',
+    label: 'Pregnancies',
+  },
+  seriousIncidents: {
+    slug: 'serious_incidents',
+    label: 'Serious Incidents',
+  },
+};
