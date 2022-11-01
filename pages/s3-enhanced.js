e2f2aa37daf16394585dc4bb8cac7b8e1bbbc267
LayaTaal <jason@zinnfinity.com>
2022-11-01 17:12:56 -0400

    setup enhanced table options

---
+++ pages/s3-enhanced.js
@@ -0,0 +1,472 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { NextSeo } from 'next-seo';
+import S3 from 'aws-sdk/clients/s3';
+import styled from 'styled-components';
+import Sidebar from '../components/Sidebar';
+import Primary from '../components/Primary';
+import Layout from '../components/Layout';
+
+import TCJSReportSchema from '../schema/tcjs-reports';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const params = {
+  Bucket: 'tcjs-reports' /* required */,
+  //   ContinuationToken: 'STRING_VALUE',
+  //   Delimiter: 'STRING_VALUE',
+  //   EncodingType: url,
+  //   ExpectedBucketOwner: 'STRING_VALUE',
+  //   FetchOwner: true || false,
+  //   MaxKeys: 2,
+  //   Prefix: 'STRING_VALUE',
+  //   RequestPayer: requester,
+  //   StartAfter: 'STRING_VALUE'
+};
+
+function createData(type, year, filename, url) {
+  return {
+    type,
+    year,
+    filename,
+    url,
+  };
+}
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// This method is created for cross-browser compatibility, if you don't
+// need to support IE11, you can use Array.prototype.sort() directly
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+const headCells = [
+  {
+    id: 'type',
+    numeric: false,
+    disablePadding: true,
+    label: 'Report Type',
+  },
+  {
+    id: 'year',
+    numeric: false,
+    disablePadding: true,
+    label: 'Year',
+  },
+  {
+    id: 'filename',
+    numeric: false,
+    disablePadding: true,
+    label: 'Filename',
+  },
+  {
+    id: 'url',
+    numeric: false,
+    disablePadding: true,
+    label: 'Download',
+  },
+];
+
+function EnhancedTableHead(props) {
+  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+  const createSortHandler = property => event => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              'aria-label': 'select all desserts',
+            }}
+          />
+        </TableCell>
+        {headCells.map((headCell, index) => (
+          <TableCell
+            key={headCell.id}
+            align={index === 0 ? 'left' : 'right'}
+            padding={index === 0 ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+function EnhancedTableToolbar(props) {
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: theme => alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography sx={{ flex: '1 1 100%' }} color="inherit" variant="subtitle1" component="div">
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography sx={{ flex: '1 1 100%' }} variant="h6" id="tableTitle" component="div">
+          Nutrition
+        </Typography>
+      )}
+
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+}
+
+export async function getServerSideProps() {
+  const newS3 = new S3({
+    region: 'us-east-1',
+    accessKeyId: process.env.TJI_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.TJI_AWS_SECRET_KEY,
+    signatureVersion: 'v4',
+  });
+
+  const res = await new Promise((resolve, reject) => {
+    newS3.listObjectsV2(params, function(err, data) {
+      if (err) reject(err, err.stack);
+      resolve(data);
+    });
+  });
+
+  return {
+    props: { data: JSON.stringify(res.Contents) },
+  };
+}
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
+
+const Content = styled.div`
+  li {
+    margin: 1em 0;
+  }
+`;
+
+export default function EnhancedTable({ data }) {
+  const [years, setYears] = React.useState([]);
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChange = event => {
+    const {
+      target: { value },
+    } = event;
+    setYears(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value
+    );
+  };
+
+  const items = JSON.parse(data);
+  const rows = [];
+
+  // Desconstruct our file path to extract some useful data from each report
+  const reformedData = items.map(item => {
+    const itemPath = item.Key.split('/');
+
+    rows.push(createData(itemPath[0], itemPath[1], itemPath[2], `https://tcjs-reports.s3.amazonaws.com/${item.Key}`));
+
+    return {
+      type: itemPath[0] ? itemPath[0] : null,
+      year: itemPath[1] ? itemPath[1] : null,
+      fileName: itemPath[2] ? itemPath[2] : null,
+      ...item,
+    };
+  });
+
+  const filteredData = rows.filter(item => years.indexOf(parseInt(item.year)) !== -1);
+
+  // Create a sorted array of all available report years
+  const availableYears = [...new Set(reformedData.map(dataItem => parseInt(dataItem.year)))].sort((a, b) => a - b);
+
+  // Create an array to help loop through each report type
+  // const reportTypes = Object.keys(TCJSReportSchema);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = event => {
+    if (event.target.checked) {
+      const newSelected = filteredData.map(n => n.name);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = event => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = name => selected.indexOf(name) !== -1;
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  console.log(filteredData);
+  return (
+    <React.Fragment>
+      <NextSeo title="S3 Fetch Test" />
+      <Layout>
+        <Primary>
+          <h1>Downloadable Reports</h1>
+          <Content>
+            <div style={{ marginBlock: '24px' }}>
+              <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel id="demo-multiple-name-label">Available Years</InputLabel>
+                <Select
+                  labelId="demo-multiple-name-label"
+                  id="demo-multiple-name"
+                  multiple
+                  value={years}
+                  onChange={handleChange}
+                  input={<OutlinedInput label="Name" />}
+                  renderValue={selectedYear => selectedYear.join(', ')}
+                  MenuProps={MenuProps}
+                >
+                  {availableYears.map(name => (
+                    <MenuItem key={name} value={name}>
+                      <Checkbox checked={years.indexOf(name) > -1} />
+                      {name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+
+            {filteredData.length > 0 && (
+              <Box sx={{ width: '100%' }}>
+                <Paper sx={{ width: '100%', mb: 2 }}>
+                  <EnhancedTableToolbar numSelected={selected.length} />
+                  <TableContainer>
+                    <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle" size={dense ? 'small' : 'medium'}>
+                      <EnhancedTableHead
+                        numSelected={selected.length}
+                        order={order}
+                        orderBy={orderBy}
+                        onSelectAllClick={handleSelectAllClick}
+                        onRequestSort={handleRequestSort}
+                        rowCount={filteredData.length}
+                      />
+                      <TableBody>
+                        {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+                 rows.sort(getComparator(order, orderBy)).slice() */}
+                        {stableSort(filteredData, getComparator(order, orderBy))
+                          .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                          .map((row, index) => {
+                            const isItemSelected = isSelected(row.name);
+                            const labelId = `enhanced-table-checkbox-${index}`;
+
+                            return (
+                              <TableRow
+                                hover
+                                onClick={event => handleClick(event, row.name)}
+                                role="checkbox"
+                                aria-checked={isItemSelected}
+                                tabIndex={-1}
+                                key={row.name}
+                                selected={isItemSelected}
+                              >
+                                <TableCell padding="checkbox">
+                                  <Checkbox
+                                    color="primary"
+                                    checked={isItemSelected}
+                                    inputProps={{
+                                      'aria-labelledby': labelId,
+                                    }}
+                                  />
+                                </TableCell>
+                                <TableCell id={labelId} scope="row" padding="none">
+                                  {TCJSReportSchema[row.type].label}
+                                </TableCell>
+                                <TableCell align="right">{row.year}</TableCell>
+                                <TableCell align="right">{row.filename}</TableCell>
+                                <TableCell align="right">
+                                  <a href={row.url} target="_blank" rel="noreferrer">
+                                    Download
+                                  </a>
+                                </TableCell>
+                              </TableRow>
+                            );
+                          })}
+                        {emptyRows > 0 && (
+                          <TableRow
+                            style={{
+                              height: (dense ? 33 : 53) * emptyRows,
+                            }}
+                          >
+                            <TableCell colSpan={6} />
+                          </TableRow>
+                        )}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                  <TablePagination
+                    rowsPerPageOptions={[10, 25, 50]}
+                    component="div"
+                    count={filteredData.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                  />
+                </Paper>
+                <FormControlLabel
+                  control={<Switch checked={dense} onChange={handleChangeDense} />}
+                  label="Dense padding"
+                />
+              </Box>
+            )}
+          </Content>
+        </Primary>
+        <Sidebar />
+      </Layout>
+    </React.Fragment>
+  );
+}
+
+EnhancedTable.propTypes = {
+  data: PropTypes.string.isRequired,
+};
