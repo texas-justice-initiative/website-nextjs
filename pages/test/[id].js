5850c6ca8a85179cfaa45853ef9b420cee87aa42
Jason Zinn <jason@zinnfinity.com>
2023-04-23 13:35:06 -0400

    updated static path testing

--- pages/test/[id].js
+++ pages/test/[id].js
@@ -9,17 +9,33 @@ const Test = () => {
 
 export default Test;
 
-export async function getStaticProps({ params }) {
-  return {
-    props: {
-      id: params.id,
-    },
-  };
-}
+// export async function getStaticProps({ params }) {
+//   return {
+//     props: {
+//       id: params.id,
+//     },
+//   };
+// }
 
+// export async function getStaticPaths() {
+//   return {
+//     paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+//     fallback: false,
+//   };
+// }
+
+// Generates `/posts/1` and `/posts/2`
 export async function getStaticPaths() {
   return {
     paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
-    fallback: false,
+    fallback: false, // can also be true or 'blocking'
+  };
+}
+
+// `getStaticPaths` requires using `getStaticProps`
+export async function getStaticProps(context) {
+  return {
+    // Passed to the page component as props
+    props: { id: context.params },
   };
 }
