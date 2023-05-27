4a0c942fad9cbf58a16f9ceb0b1243228d16d4c0
Jason Zinn <jason@zinnfinity.com>
2023-05-27 11:48:50 -0400

    migrated about the data

---
+++ app/about-the-data/about-the-data.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React from 'react';
+import styled from 'styled-components';
+import Layout from '@/components/Layout';
+import Primary from '@/components/Primary';
+import Sidebar from '@/components/Sidebar';
+
+function AboutTheData(props) {
+  const { content } = props;
+  const {
+    html,
+    attributes: { title },
+  } = content;
+  return (
+    <Layout>
+      <Primary>
+        <h1>{title}</h1>
+        <Content dangerouslySetInnerHTML={{ __html: html }} />
+      </Primary>
+      <Sidebar />
+    </Layout>
+  );
+}
+
+export default AboutTheData;
+
+const Content = styled.div`
+  img {
+    width: 100%;
+    max-width: 600px;
+    height: auto;
+    padding: 3rem 0;
+  }
+`;
