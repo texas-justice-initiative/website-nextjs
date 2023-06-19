0935878559571be26ede4a65db0fcff740dea353
LayaTaal <jason@zinnfinity.com>
2023-06-19 10:39:01 -0400

    updated import

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 class VideoPlayer extends React.Component {
   constructor(props) {
