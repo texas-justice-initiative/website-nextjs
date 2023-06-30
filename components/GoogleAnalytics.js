9bb2dbd29f01ae64a938d3a980c58a7b7cb3f8cb
LayaTaal <jason@zinnfinity.com>
2023-06-30 08:53:30 -0500

    refactored analytics

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -1,37 +1,17 @@
-import { useEffect } from 'react';
-import router from 'next/router';
-import ReactGA from 'react-ga4';
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA4_ID;
 
-const initGA = () => {
-  ReactGA.initialize('G-28GPT3YGPX');
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
 };
 
-const LogPageView = (url) => {
-  ReactGA.set({ page: url });
-  ReactGA.pageview(url);
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
 };
-
-/**
- * Next.js no longer pre-renders router.events and therefore accessing it
- * now needs to occur through the useEffect hook.
- *
- * Next.js documentation: https://nextjs.org/docs/upgrading#update-usage-of-routerevents
- */
-export default function GoogleAnalytics() {
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      initGA();
-      LogPageView(url);
-    };
-
-    router.events.on('routeChangeStart', handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method:
-    return () => {
-      router.events.off('routeChangeStart', handleRouteChange);
-    };
-  }, []);
-
-  return null;
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Page from '../components/Page';
 import { Roboto_Flex } from 'next/font/google';
+import { useRouter } from 'next/router';
+import * as gtag from '../components/GoogleAnalytics';
+import Script from 'next/script';
 
 const roboto = Roboto_Flex({ subsets: ['latin'] });
 
@@ -11,9 +14,38 @@ export declare interface AppProps {
 
 export default function TJIApp(props: AppProps) {
   const { Component, pageProps } = props;
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <div className={roboto.className}>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Page>
         <Component {...pageProps} />
       </Page>
