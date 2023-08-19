b2f7767d8f7c070e73814667cb11e25cecbe5423
Jason Zinn <jason@zinnfinity.com>
2023-08-18 21:03:55 -0500

    tests

--- app/topics/[topic]/page.tsx
+++ app/topics/[topic]/page.tsx
@@ -22,15 +22,15 @@ function Fallback() {
 }
 
 function Page({ params }: { params: { topic: string } }) {
-  // const posts: MarkdownFile[] = useMarkdownFiles('./content/blog/posts/').data;
-  // const topics: MarkdownFile[] = useMarkdownFiles(
-  //   './content/blog/topics/'
-  // ).data;
-  // const authors: MarkdownFile[] = useMarkdownFiles(
-  //   './content/blog/authors/'
-  // ).data;
-
   return <p>Loading</p>;
+
+  const posts: MarkdownFile[] = useMarkdownFiles('./content/blog/posts/').data;
+  const topics: MarkdownFile[] = useMarkdownFiles(
+    './content/blog/topics/'
+  ).data;
+  const authors: MarkdownFile[] = useMarkdownFiles(
+    './content/blog/authors/'
+  ).data;
 
   // const currentTopic = topics.filter((topic) => topic.slug === params.topic)[0];
 
