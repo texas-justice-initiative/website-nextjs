e6bae4730484f50e05609dc93f5678587d86dc5a
LayaTaal <jason@zinnfinity.com>
2023-09-22 10:31:38 -0500

    added content layer

---
+++ contentlayer.config.ts
@@ -0,0 +1,22 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.md`,
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (post) => `/post/${post._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: 'content/blog/posts',
+  documentTypes: [Post],
+});
