33128ce710c1ef7d62da7318f5b151f0317812b7
Jason Zinn <jason@zinnfinity.com>
2023-10-03 14:52:46 -0500

    updated donate button

--- app/donate/donate.tsx
+++ app/donate/donate.tsx
@@ -8,55 +8,139 @@ import {
 } from '@paypal/react-paypal-js';
 import DonateHero from '@/components/DonateHero';
 import Primary from '@/components/Primary';
+import { useState } from 'react';
+import {
+  RadioProvider,
+  RadioGroup,
+  Radio,
+  useRadioStore,
+} from '@ariakit/react';
+import { useRouter } from 'next/navigation';
 
 const initialOptions = {
   clientId:
-    'AZ2LDJwEbuFjH45Izqk5pmxHtyzxtooUPBCrvrn7tjKXIbv-xGxXsflhCMGl6dy2tRBEliztwiPzCckc',
+    'AdjzG--Uaf6pzaZ0Z5Dec77AF8k0zsUTyVfjY3tl_w_5PcgP5ZYcNXu2bZupZV2ZLTUWYWmsvK26BnpK', // TJI Sandbox
   currency: 'USD',
   intent: 'capture',
 };
 
-function ButtonWrapper() {
+function DonationForm() {
   const [{ isPending }] = usePayPalScriptReducer();
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const donationAmount = useRadioStore();
+  const router = useRouter();
+  const [error, setError] = useState<{ field: string; message: string } | null>(
+    null
+  );
+
+  const handleCreateOrder = (data, actions) => {
+    const value = donationAmount.getState().value;
+
+    if (!value) {
+      setError({
+        field: 'amount',
+        message: 'Please select an amount',
+      });
+    }
+
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value,
+            currency_code: 'USD',
+            breakdown: {
+              item_total: {
+                value,
+                currency_code: 'USD',
+              },
+            },
+          },
+          items: [
+            {
+              name: 'One time donation',
+              quantity: '1',
+              unit_amount: {
+                currency_code: 'USD',
+                value,
+              },
+              category: 'DONATION',
+            },
+          ],
+        },
+      ],
+    });
+  };
+
+  const handleApprove = async (data, actions) => {
+    router.push('/thanks');
+  };
 
   return (
     <div>
       {isPending && <div className="spinner" />}
+      <div>
+        <label htmlFor="first-name">First Name</label>
+        <input
+          id="first-name"
+          name="first-name"
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="last-name">Last Name</label>
+        <input
+          id="last-name"
+          name="last-name"
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <RadioGroup store={donationAmount}>
+        <label className="label">
+          <Radio className="radio" value="5" />
+          $5
+        </label>
+        <label className="label">
+          <Radio className="radio" value="10" />
+          $10
+        </label>
+        <label className="label">
+          <Radio className="radio" value="15" />
+          $15
+        </label>
+        <label className="label">
+          <Radio className="radio" value="25" />
+          $25
+        </label>
+      </RadioGroup>
+      {error?.field === 'amount' && (
+        <p style={{ color: 'red' }}>{error.message}</p>
+      )}
+
       <PayPalButtons
         fundingSource="paypal"
         style={{ layout: 'vertical', label: 'donate' }}
-        disabled={false}
+        // disabled={!donationAmount.getState().value}
         //   forceReRender={[style]}
-        createOrder={(data, actions) => {
-          return actions.order.create({
-            purchase_units: [
-              {
-                amount: {
-                  value: '2',
-                  currency_code: 'USD',
-                  breakdown: {
-                    item_total: {
-                      value: '2',
-                      currency_code: 'USD',
-                    },
-                  },
-                },
-                items: [
-                  {
-                    name: 'donation-example',
-                    quantity: '1',
-                    unit_amount: {
-                      currency_code: 'USD',
-                      value: '2',
-                    },
-                    category: 'DONATION',
-                  },
-                ],
-              },
-            ],
-          });
-        }}
-        //   onApprove={onApprove}
+        createOrder={handleCreateOrder}
+        onApprove={handleApprove}
       />
     </div>
   );
@@ -71,7 +155,7 @@ export function Donate() {
       <Layout>
         <Primary>
           <PayPalScriptProvider options={initialOptions}>
-            <ButtonWrapper />
+            <DonationForm />
           </PayPalScriptProvider>
         </Primary>
       </Layout>
